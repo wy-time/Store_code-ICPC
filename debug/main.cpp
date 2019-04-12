99f8ea87e08f2c6c42b953a6b1066384ac8728ba
wy-time <1142942494@qq.com>
2019-04-13 01:02:25 +0800

    CU

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,193 +1,51 @@
 #include <iostream>
-#include <set>
-#include <cstring>
+#include <cmath> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn = 1005;
-int vis[maxn];
-struct st
-{
-    int end;
-    int next;
-};
-st edge[maxn * 20];
-int head[maxn];
-int cnt;
-void add(int beg, int end)
-{
-    edge[++cnt].next = head[beg];
-    edge[cnt].end = end;
-    head[beg] = cnt;
-}
-int have[maxn];
-int dfs(int beg)
-{
-    int i;
-    for (i = head[beg]; i; i = edge[i].next)
-    {
-        int v = edge[i].end;
-        if (vis[v] == 0)
-        {
-            vis[v] = vis[beg] * -1;
-            if (dfs(v) == 0)
-                return 0;
-        } else
-        {
-            if (vis[v] == vis[beg] * -1)
-                continue;
-            if (vis[v] < -1 && vis[beg] > 0)
-            {
-                vis[v] = -1;
-                if (dfs(v) == 0)
-                    return 0;
-                continue;
-            }
-            if (vis[v] > 1 && vis[beg] < 0)
-            {
-                vis[v] = 1;
-                if (dfs(v) == 0)
-                    return 0;
-                continue;
-            }
-            return 0;
-        }
-    }
-    return 1;
-}
-int num[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
-#ifdef test
-    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-#endif
-#ifdef ubuntu
-    freopen("/home/time/debug/debug/in", "r", stdin);
-    freopen("/home/time/debug/debug/out", "w", stdout);
-#endif
-    int n, m, x, y;
-    while (cin >> n >> m >> x >> y)
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
+    int t;
+    cin>>t;
+    while(t--)
     {
-        set<pair<int, int>>st;
-        cnt = 0;
-        memset(vis, 0, sizeof(vis));
-        memset(head, 0, sizeof(head));
-        memset(have, 0, sizeof(have));
-        int i;
-        wfor(i, 0, m)
+        ll l,r;
+        cin>>l>>r;
+        //int high=log2(l);
+        ll ans=0;
+        ll i;
+        wfor(i,0,62)
         {
-            int u, v;
-            cin >> u >> v;
-            if (st.count(make_pair(u, v)) == 0)
+            ll temp=1ll<<i;
+            if(temp>r)
+                break;
+            ans+=temp;
+            if(ans>r)
             {
-                add(u, v);
-                add(v, u);
-                have[u] = 1;
-                have[v] = 1;
-                st.insert(make_pair(u, v));
-                st.insert(make_pair(v, u));
+                ans-=temp;
+                break;
             }
         }
-        int tnum = 0;
-        wfor(i, 0, x)
-        {
-            int t;
-            cin >> t;
-            num[tnum++] = t;
-            vis[t] = 2;
-        }
-        wfor(i, 0, y)
-        {
-            int t;
-            cin >> t;
-            num[tnum++] = t;
-            vis[t] = -2;
-        }
-        int flag = 1;
-        wfor(i, 1, n + 1)
-        {
-            if (have[i] && vis[i] != -1 && vis[i] != 1)
-            {
-                if (vis[i] == 0)
-                    vis[i] = 1;
-                else if (vis[i] == 2)
-                    vis[i] = 1;
-                else if (vis[i] == -2)
-                    vis[i] = -1;
-                if (dfs(i) == 0)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-        }
-        if (flag)
-        {
-            wfor(i, 1, n + 1)
-            {
-                if (vis[i] == 0)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-            if (flag)
-                cout << "YES" << endl;
-        }
-        if (flag == 0)
-        {
-            memset(vis, 0, sizeof(vis));
-            wfor(i, 0, x)
-            {
-                vis[num[i]] = 2;
-            }
-            wfor(i, x, x + y)
-            {
-                vis[num[i]] = -2;
-            }
-            flag = 1;
-            wfor(i, 1, n + 1)
-            {
-                if (have[i] && vis[i] != -1 && vis[i] != 1)
-                {
-                    if (vis[i] == 0)
-                        vis[i] = -1;
-                    else if (vis[i] == 2)
-                        vis[i] = 1;
-                    else if (vis[i] == -2)
-                        vis[i] = -1;
-                    if (dfs(i) == 0)
-                    {
-                        flag = 0;
-                        break;
-                    }
-                }
-            }
-            if (flag)
-            {
-                wfor(i, 1, n + 1)
-                {
-                    if (vis[i] == 0)
-                    {
-                        flag = 0;
-                        break;
-                    }
-                }
-                if (flag)
-                    cout << "YES" << endl;
-                else
-                    cout <<"NO"<<endl;
-            } else
-                cout << "NO" << endl;
-        }
+        if(ans>=l)
+            ans=ans|r;
+        else
+            ans=r;
+        cout<<ans<<endl;
     }
     return 0;
 }
