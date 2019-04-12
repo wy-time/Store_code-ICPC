84f9ae496ecd51214eab5827e9ecee3e6f9aa91c
wy-time <1142942494@qq.com>
2019-04-13 01:08:51 +0800

    CU

--- VJ topic/铜牌题/IA.cpp
+++ VJ topic/铜牌题/IA.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <set>
 #include <cstring>
-#include <vector>
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -11,30 +11,58 @@ typedef long long ll;
 //  for (; ch < '0' || ch > '9'; ch = getchar());
 //  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn = 10005;
-vector<int>G[maxn];
+const int maxn = 1005;
 int vis[maxn];
-int maching[maxn];
-int slove(int beg)
+struct st
+{
+    int end;
+    int next;
+};
+st edge[maxn * 20];
+int head[maxn];
+int cnt;
+void add(int beg, int end)
+{
+    edge[++cnt].next = head[beg];
+    edge[cnt].end = end;
+    head[beg] = cnt;
+}
+int have[maxn];
+int dfs(int beg)
 {
     int i;
-    int len = G[beg].size();
-    wfor(i, 0, len)
+    for (i = head[beg]; i; i = edge[i].next)
     {
-        int v = G[beg][i];
-        if (!vis[v])
+        int v = edge[i].end;
+        if (vis[v] == 0)
         {
-            vis[v] = 1;
-            if (maching[v] == -1 || slove(maching[v]))
+            vis[v] = vis[beg] * -1;
+            if (dfs(v) == 0)
+                return 0;
+        } else
+        {
+            if (vis[v] == vis[beg] * -1)
+                continue;
+            if (vis[v] < -1 && vis[beg] > 0)
             {
-                maching[v] = beg;
-                maching[beg] = v;
-                return 1;
+                vis[v] = -1;
+                if (dfs(v) == 0)
+                    return 0;
+                continue;
             }
+            if (vis[v] > 1 && vis[beg] < 0)
+            {
+                vis[v] = 1;
+                if (dfs(v) == 0)
+                    return 0;
+                continue;
+            }
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
+int num[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -48,46 +76,118 @@ int main()
     int n, m, x, y;
     while (cin >> n >> m >> x >> y)
     {
+        set<pair<int, int>>st;
+        cnt = 0;
+        memset(vis, 0, sizeof(vis));
+        memset(head, 0, sizeof(head));
+        memset(have, 0, sizeof(have));
         int i;
-        wfor(i, 0, n + 1)
-        G[i].clear();
         wfor(i, 0, m)
         {
             int u, v;
             cin >> u >> v;
-            G[u].push_back(v + n);
-            G[v + n].push_back(u);
-        }
-        memset(maching, -1, sizeof(maching));
-        int ans = 0;
-        wfor(i, 1, n + 1)
-        {
-            if (maching[i] == -1)
+            if (st.count(make_pair(u, v)) == 0)
             {
-                memset(vis, 0, sizeof(vis));
-                if (slove(i) == 1)
-                    ans++;
+                add(u, v);
+                add(v, u);
+                have[u] = 1;
+                have[v] = 1;
+                st.insert(make_pair(u, v));
+                st.insert(make_pair(v, u));
             }
         }
-        ans *= 2;
+        int tnum = 0;
         wfor(i, 0, x)
         {
             int t;
             cin >> t;
-            if (maching[t] == -1)
-                ans++;
+            num[tnum++] = t;
+            vis[t] = 2;
         }
         wfor(i, 0, y)
         {
             int t;
             cin >> t;
-            if (maching[t] == -1)
-                ans++;
+            num[tnum++] = t;
+            vis[t] = -2;
         }
-        if (ans == n)
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        int flag = 1;
+        wfor(i, 1, n + 1)
+        {
+            if (have[i] && vis[i] != -1 && vis[i] != 1)
+            {
+                if (vis[i] == 0)
+                    vis[i] = 1;
+                else if (vis[i] == 2)
+                    vis[i] = 1;
+                else if (vis[i] == -2)
+                    vis[i] = -1;
+                if (dfs(i) == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        if (flag)
+        {
+            wfor(i, 1, n + 1)
+            {
+                if (vis[i] == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag)
+                cout << "YES" << endl;
+        }
+        if (flag == 0)
+        {
+            memset(vis, 0, sizeof(vis));
+            wfor(i, 0, x)
+            {
+                vis[num[i]] = 2;
+            }
+            wfor(i, x, x + y)
+            {
+                vis[num[i]] = -2;
+            }
+            flag = 1;
+            wfor(i, 1, n + 1)
+            {
+                if (have[i] && vis[i] != -1 && vis[i] != 1)
+                {
+                    if (vis[i] == 0)
+                        vis[i] = -1;
+                    else if (vis[i] == 2)
+                        vis[i] = 1;
+                    else if (vis[i] == -2)
+                        vis[i] = -1;
+                    if (dfs(i) == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+            }
+            if (flag)
+            {
+                wfor(i, 1, n + 1)
+                {
+                    if (vis[i] == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag)
+                    cout << "YES" << endl;
+                else
+                    cout << "NO" << endl;
+            } else
+                cout << "NO" << endl;
+        }
     }
     return 0;
 }
