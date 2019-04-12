7504160e830179cc9e75efa5a14331669b7b0449
wy-time <1142942494@qq.com>
2019-04-12 19:16:34 +0800

    cf-1099-D

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string> 
+#include <vector> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -10,6 +10,41 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+const int maxn=2e5+5;
+ll num[maxn];
+vector<int>G[maxn];
+int dep[maxn];
+ll ans;
+int vis[maxn];
+int dfs(int beg,ll sum)
+{
+    int i;
+    int len=G[beg].size();
+    wfor(i,0,len)
+    {
+        int v=G[beg][i];
+        ll val=0;
+        if(num[v]==-1)
+            val=0;
+        else
+        {
+            val=num[v]-sum;
+            if(val<0)
+            {
+                ans=-1;
+                return 0;
+            }
+        }
+        if(!vis[v])
+        {
+            ans+=val;
+            vis[v]=1;
+            if(dfs(v,max(num[v],num[beg]))==0)
+                return 0;
+        }
+    }
+    return 1;
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -20,55 +55,24 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    string s;
-    cin>>s;
-    int k;
-    cin>>k;
+    int n;
+    cin>>n;
     int i;
-    int len=s.size();
-    string ans="";
-    int now=0;
-    wfor(i,0,len)
+    dep[1]=1;
+    wfor(i,1,n)
     {
-        if(s[i]=='?'||s[i]=='*')
-            now++;
+        int t;
+        cin>>t;
+        G[t].push_back(i+1);
     }
-    now=len-now;
-    wfor(i,0,len)
+    wfor(i,0,n)
     {
-        if(s[i+1]=='*')
-        {
-            if(k>now)
-            {
-                int j;
-                wfor(j,now,k+1)
-                {
-                    ans+=s[i];
-                }
-                now=k;
-            }else if(k<now)
-            {
-                now--;
-                continue;
-            }else
-                ans+=s[i];
-        }else if(s[i+1]=='?')
-        {
-            if(k>now)
-            {
-                ans+=s[i];
-            }else if(k<now)
-            {
-                now--;
-                continue;
-            }else
-                ans+=s[i];
-        }else if(s[i]!='*'&&s[i]!='?')
-            ans+=s[i];
+        int t;
+        cin>>t;
+        num[i+1]=t;
     }
-    if(now!=k)
-        cout<<"Impossible"<<endl;
-    else
-        cout<<ans<<endl;
+    ans=num[1];
+    dfs(1,num[1]);
+    cout<<ans<<endl;
     return 0;
 }
