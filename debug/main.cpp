1a7efbb466d0056e10c7312d2e5ab2db90c48fd5
wy-time <1142942494@qq.com>
2019-04-13 19:33:35 +0800

    2050

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,49 +1,63 @@
 #include <iostream>
-#include <string>
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
 int main()
 {
-    std::ios::sync_with_stdio(false);
-#ifdef test
-    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-#endif
-#ifdef ubuntu
-    freopen("/home/time/debug/debug/in", "r", stdin);
-    freopen("/home/time/debug/debug/out", "w", stdout);
-#endif
+    //std::ios::sync_with_stdio(false);
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
     int n;
-    cin >> n;
-    string s2 = "2050";
-    while (n--)
+    cin>>n;
+    int mou[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    while(n--)
     {
-        string s;
-        cin >> s;
-        int len = s.size();
+        int y,m,d,h,mi,s;
+        scanf("%d-%d-%d %d:%d:%d",&y,&m,&d,&h,&mi,&s);
         int i;
-        int flag = 1;
-        wfor(i, 0, len)
+        ll ans=0;
+        wfor(i,y+1,2050)
         {
-            if (s[i] != s2[i % 4])
+            if((i%4==0&&i%100!=0)||(i%400==0))
             {
-                flag = 0;
-                break;
-            }
+                ans+=366;
+            }else
+                ans+=365;
         }
-        if (len % 4 != 0)
-            flag = 0;
-        if (flag == 1)
-            cout << "Yes" << endl;
-        else
-            cout << "No" << endl;
+        if((y%4==0&&y%100!=0)||y%400==0)
+        {
+            if(m<2)
+                ans++;
+            wfor(i,m+1,12)
+                ans+=mou[i];
+            if(m==2)
+                ans+=29-d-1;
+            else
+                ans+=mou[m]-d-1;
+        }else
+        {
+            wfor(i,m+1,12)
+                ans+=mou[i];
+            ans+=mou[m]-d-1;
+        }
+        ans=ans%100*24%100*60%100*60%100;
+        int temp=h*3600+mi*60+s;
+        temp=12*3600-temp;
+        ans+=temp;
+        ans%=100;
+        cout<<ans<<endl;
     }
     return 0;
 }
