4fcae8139387d736b316c53f17e2234eddba7ed8
wy-time <1142942494@qq.com>
2019-04-14 14:28:44 +0800

    cf-1153-A

--- debug/main.cpp
+++ debug/main.cpp
@@ -4,94 +4,46 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(ll &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// void read(int &x) {
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
 int main()
 {
     std::ios::sync_with_stdio(false);
-#ifdef test
-    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-#endif
-#ifdef ubuntu
-    freopen("/home/time/debug/debug/in", "r", stdin);
-    freopen("/home/time/debug/debug/out", "w", stdout);
-#endif
-    ll t;
-    cin >> t;
-    while (t--)
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
+    int n,t;
+    cin>>n>>t;
+    int i;
+    int ans=1e9;
+    int res=0;
+    wfor(i,0,n)
     {
-        ll n, m, k, a, b, c;
-        cin >> n >> m >> k >> a >> b >> c;
-        ll i, j;
-        ll ans = 1e17;
-        wfor(i, 0, k + 1)
+        int temp=t;
+        int a,b;
+        cin>>a>>b;
+        if(t>a)
         {
-            ll na = n + k - i;
-            ll nv = m + k - i;
-            ll te1 = 1e17;
-            ll te2 = 1e17;
-            ll j1 = na;
-            ll j2 = nv;
-            wfor(j, 0, j1 + 1)
-            {
-                na = n + k - i;
-                ll temp = 0;
-                temp += j * a;
-                na -= 2 * j;
-                na = max(0ll, na);
-                temp += na / 3 * b;
-                temp += na % 3 == 0 ? 0 : b;
-                te1 = min(temp, te1);
-            }
-            wfor(j, 0, j2 + 1)
-            {
-                nv = m + k - i;
-                ll temp = 0;
-                temp += j * a;
-                nv -= 2 * j;
-                nv = max(0ll, nv);
-                temp += nv / 3 * b;
-                temp += nv % 3 == 0 ? 0 : b;
-                te2 = min(temp, te2);
-            }
-            ans = min(ans, te1 + te2 + i * c);
-            // ll need1 = na / 2 + na % 2;
-            // ll need2 = nv / 2 + nv % 2;
-            // ll need = max(need1, need2);
-            // wfor(j, 0, need + 2)
-            // {
-            //     ll temp = i * c;
-            //     na = n + k - i;
-            //     nv = m + k - i;
-            //     if (na >= 2 * j)
-            //     {
-            //         temp += j * a;
-            //         na -= 2 * j;
-            //         temp += na / 3 * b;
-            //         temp += na % 3 == 0 ? 0 : b;
-            //     } else
-            //     {
-            //         temp += na / 2 * a;
-            //         temp += na % 2 * a;
-            //     }
-            //     if (nv >= 2 * j)
-            //     {
-            //         temp += j * a;
-            //         nv -= j * 2;
-            //         temp += nv / 3 * b;
-            //         temp += nv % 3 == 0 ? 0 : b;
-            //     } else
-            //     {
-            //         temp += nv / 2 * a;
-            //         temp += nv % 2 * a;
-            //     }
-            //     ans = min(ans, temp);
-            // }
+            temp-=a;
+            if(temp%b==0)
+                temp/=b;
+            else
+                temp=temp/b+1;
+        }else
+            temp=0;
+        if(temp*b+a<ans)
+        {
+            ans=temp*b+a;
+            res=i+1;
         }
-        cout << ans << endl;
     }
+    cout<<res<<endl;
     return 0;
 }
