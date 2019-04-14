783ba4f9d79a2ea34081edea6ce8a4212c7091a6
wy-time <1142942494@qq.com>
2019-04-14 15:34:32 +0800

    cf

---
+++ cf/div2/551/B.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+typedef long long ll;
+#define wfor(i,j,k) for(i=j;i<k;++i)
+#define mfor(i,j,k) for(i=j;i>=k;--i)
+// void read(int &x) {
+//  char ch = getchar(); x = 0;
+//  for (; ch < '0' || ch > '9'; ch = getchar());
+//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// }
+int ma[105][105];
+int numf[105];
+int numl[105];
+int main()
+{
+    std::ios::sync_with_stdio(false);
+#ifdef test
+    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
+#endif
+#ifdef ubuntu
+    freopen("/home/time/debug/debug/in", "r", stdin);
+    freopen("/home/time/debug/debug/out", "w", stdout);
+#endif
+    int n, m, h;
+    cin >> n >> m >> h;
+    int i, j;
+    wfor(i, 0, m)
+    {
+        cin >> numf[i];
+    }
+    wfor(i, 0, n)
+    {
+        cin >> numl[i];
+    }
+    wfor(i, 0, n)
+    {
+        wfor(j, 0, m)
+        {
+            cin >> ma[i][j];
+        }
+    }
+    wfor(i, 0, n)
+    {
+        wfor(j, 0, m)
+        {
+            if (ma[i][j] == 1)
+                ma[i][j] = min(numl[i], numf[j]);
+        }
+    }
+    wfor(i, 0, n)
+    {
+        wfor(j, 0, m)
+        {
+            cout << ma[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
