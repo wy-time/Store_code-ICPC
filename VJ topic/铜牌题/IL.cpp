283e4f078831aab13ce2a3499c9beb4062721e32
wy-time <1142942494@qq.com>
2019-04-16 22:55:47 +0800

    cu

---
+++ VJ topic/铜牌题/IL.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+typedef long long ll;
+#define wfor(i,j,k) for(i=j;i<k;++i)
+#define mfor(i,j,k) for(i=j;i>=k;--i)
+// void read(int &x) {
+//  char ch = getchar(); x = 0;
+//  for (; ch < '0' || ch > '9'; ch = getchar());
+//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// }
+int main()
+{
+    std::ios::sync_with_stdio(false);
+#ifdef test
+    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
+#endif
+#ifdef ubuntu
+    freopen("/home/time/debug/debug/in", "r", stdin);
+    freopen("/home/time/debug/debug/out", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll ans = 0;
+        ll a, b;
+        cin >> a >> b;
+        ans = max(a, b) * 2 + min(a, b);
+        cout << ans << endl;
+    }
+    return 0;
+}
