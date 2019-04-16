2395d99eb0fbb9093ddf55d8062b3148a65e3203
wy-time <1142942494@qq.com>
2019-04-16 22:42:13 +0800

    cu

--- VJ topic/铜牌题/IC.cpp
+++ VJ topic/铜牌题/IC.cpp
@@ -5,13 +5,13 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-void read(ll &x) {
-    char ch = getchar(); x = 0;
-    for (; ch < '0' || ch > '9'; ch = getchar());
-    for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-}
+// void read(int &x) {
+//  char ch = getchar(); x = 0;
+//  for (; ch < '0' || ch > '9'; ch = getchar());
+//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// }
 const ll mod = 1e9 + 7;
-const ll maxn = 1e5 + 5;
+const int maxn = 1e5 + 5;
 ll fac[maxn];
 ll inv[maxn];
 ll sum[maxn + 1];
@@ -30,7 +30,7 @@ ll ksm(ll a, ll b)
 void init()
 {
     fac[1] = fac[0] = inv[0] = inv[1] = 1;
-    ll i;
+    int i;
     wfor(i, 2, maxn)
     {
         fac[i] = fac[i - 1] * i % mod;
@@ -48,30 +48,27 @@ void init()
 }
 int main()
 {
-//     std::ios::sync_with_stdio(false);
-// #ifdef test
-//     freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-// #endif
-// #ifdef ubuntu
-//     freopen("/home/time/debug/debug/in", "r", stdin);
-//     freopen("/home/time/debug/debug/out", "w", stdout);
-// #endif
-    ll t;
-    read(t);
-    // cin >> t;
+    std::ios::sync_with_stdio(false);
+#ifdef test
+    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
+#endif
+#ifdef ubuntu
+    freopen("/home/time/debug/debug/in", "r", stdin);
+    freopen("/home/time/debug/debug/out", "w", stdout);
+#endif
+    int t;
+    cin >> t;
     init();
     while (t--)
     {
         ll n;
-        // cin >> n;
-        read(n);
+        cin >> n;
         if (n == 1)
         {
-            // cout << 1 << endl;
-            printf("1\n");
+            cout << 1 << endl;
             continue;
         }
-        ll pos = lower_bound(sum + 1, sum + 1 + maxn, n) - sum;
+        int pos = lower_bound(sum + 1, sum + 1 + maxn, n) - sum;
         ll x = 0;
         ll ans = 0;
         if (sum[pos] == n)
@@ -86,7 +83,7 @@ int main()
             ll N = 0;
             ll cha;
             cha = sum[pos] - n;
-            ll fixpos = lower_bound(sum + 1, sum + 1 + maxn, cha) - sum;
+            int fixpos = lower_bound(sum + 1, sum + 1 + maxn, cha) - sum;
             if (sum[fixpos] == cha)
             {
                 x = fixpos + 1;
@@ -96,14 +93,13 @@ int main()
             {
                 cha = n - sum[pos - 1];
                 ll fix = cha + 1;
-                ll temp = fix / (pos - 2);
+                int temp = fix / (pos - 2);
                 fix %= (pos - 2);
                 ans = fac[pos - 1 - fix + temp] * inv[pos - 2 - fix + temp] % mod;
-                ans = ans * fac[pos + temp] % mod * inv[pos + temp - fix] % mod;
+                ans = ans % mod * fac[pos + temp] % mod * inv[pos + temp - fix] % mod;
             }
         }
-        // cout << ans % mod << endl;
-        printf("%lld\n", ans % mod);
+        cout << ans % mod << endl;
     }
     return 0;
 }
