7c1cbc233e0ad5e2b707277e2546b3ae32a8e9b6
wy-time <1142942494@qq.com>
2019-04-18 18:47:57 +0800

    cu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,55 +1,40 @@
 #include <iostream>
-#include <cmath>
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-ll gcd(ll a, ll b)
-{
-    return b == 0 ? a : gcd(b, a % b);
-}
 int main()
 {
     std::ios::sync_with_stdio(false);
-// #ifdef test
-//     freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-// #endif
-// #ifdef ubuntu
-//     freopen("/home/time/debug/debug/in", "r", stdin);
-//     freopen("/home/time/debug/debug/out", "w", stdout);
-// #endif
-    ll a, b;
-    while (cin >> a >> b)
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
+    int n;
+    cin>>n;
+    int i;
+    int temp=255;
+    int cnt=0;
+    wfor(i,0,n)
     {
-        int flag = 0;
-        ll x, y;
-        ll g=gcd(a,b);
-        b*=g;
-        if (a * a - 4ll * b >= 0)
+        ll t;
+        cin>>t;
+        while(t)
         {
-            ll dealt = floor(sqrt(1.0 * a * a - 4 * b + 0.5));
-            if (dealt * dealt == (a * a - 4 * b))
-            {
-                if((a-dealt)%2!=0||(a+dealt)%2!=0)
-                {
-                    continue;
-                }
-                x = (a - dealt) / (2);
-                y=(a+dealt)/2;
-                flag = 1;
-            }
+            if((t&temp)==97)
+                cnt++;
+            t>>=8;
         }
-        if (flag == 1)
-        {
-            cout << min(x, y) << " " << max(x, y) << endl;
-        } else
-            cout << "No Solution" << endl;
     }
+    cout<<cnt<<endl;
     return 0;
 }
