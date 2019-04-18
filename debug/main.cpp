ce802ea398c078b6657bb958faf21331166e1330
wy-time <1142942494@qq.com>
2019-04-18 16:29:23 +0800

    cu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstring> 
+#include <cmath> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -10,46 +10,9 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const ll mod=2147493647;
-struct Matrix
+ll gcd(ll a,ll b)
 {
-    ll ma[7][7];
-};
-Matrix mul(Matrix a,Matrix b)
-{
-    Matrix c;
-    memset(c.ma,0,sizeof(c.ma));
-    int i,j,k;
-    wfor(i,0,7)
-    {
-        wfor(j,0,7)
-        {
-            wfor(k,0,7)
-            {
-                c.ma[i][j]+=a.ma[i][k]*b.ma[k][j]%mod;
-                c.ma[i][j]%=mod;
-            }
-        }
-    }
-    return c;
-}
-Matrix ksm(Matrix a,ll b)
-{
-    Matrix ans;
-    memset(ans.ma,0,sizeof(ans.ma));
-    int i;
-    wfor(i,0,7)
-    {
-        ans.ma[i][i]=1;
-    }
-    while(b)
-    {
-        if(b&1)
-            ans=mul(ans,a);
-        a=mul(a,a);
-        b>>=1;
-    }
-    return ans;
+    return b==0?a:gcd(b,a%b);
 }
 int main()
 {
@@ -61,65 +24,54 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int t;
-    cin>>t;
-    Matrix ori;
-    memset(ori.ma,0,sizeof(ori.ma));
-    ori.ma[0][0]=1;
-    ori.ma[0][1]=1;
-    ori.ma[1][0]=2;
-    ori.ma[2][0]=1;
-    ori.ma[2][2]=1;
-    ori.ma[3][0]=4;
-    ori.ma[3][2]=4;
-    ori.ma[3][3]=1;
-    ori.ma[4][0]=6;
-    ori.ma[4][2]=6;
-    ori.ma[4][3]=3;
-    ori.ma[4][4]=1;
-    ori.ma[5][0]=4;
-    ori.ma[5][2]=4;
-    ori.ma[5][3]=3;
-    ori.ma[5][4]=2;
-    ori.ma[5][5]=1;
-    ori.ma[6][0]=1;
-    ori.ma[6][2]=1;
-    ori.ma[6][3]=1;
-    ori.ma[6][4]=1;
-    ori.ma[6][5]=1;
-    ori.ma[6][6]=1;
-    int fir[7];
-    fir[2]=16;
-    fir[3]=8;
-    fir[4]=4;
-    fir[5]=2;
-    fir[6]=1;
-    while(t--)
+    ll a,b;
+    while(cin>>a>>b)
     {
-        ll n;
-        cin>>n;
-        int a,b;
-        cin>>a>>b;
-        ll ans=0;
-        fir[0]=b%mod;
-        fir[1]=a%mod;
-        if(n>2)
+        int flag=0;
+        ll i;
+        ll x,y;
+        for(i=2;i*i<=a;i++)
         {
-            Matrix temp=ksm(ori,n-2);
-            int i;
-            wfor(i,0,7)
+            if(a%i==0)
             {
-                ans+=fir[i]*temp.ma[i][0];
-                ans%=mod;
+                ll c=i*b;
+                if(a*a-4*c>=0)
+                {
+                    ll dealt=floor(sqrt(1.0*a*a-4*c+0.5));
+                    if(dealt*dealt==(a*a-4*c))
+                    {
+                        flag=1;
+                        x=(-a-dealt)/(-2);
+                        y=a-x;
+                        if(gcd(x,y)!=i)
+                            continue;
+                        break;
+                    }
+                }
+                if(i*i!=a)
+                {
+                    c=(a/i)*b;
+                    if(a*a-4*c>=0)
+                    {
+                        ll dealt=floor(sqrt(1.0*a*a-4*c+0.5));
+                        if(dealt*dealt==(a*a-4*c))
+                        {
+                            flag=1;
+                            x=(-a-dealt)/(-2);
+                            y=a-x;
+                            if(gcd(x,y)!=i)
+                                continue;
+                            break;
+                        }
+                    }
+                }
             }
-            cout<<ans<<endl;
-        }else
-        {
-            if(n==1)
-                cout<<a%mod<<endl;
-            else
-                cout<<b%mod<<endl;
         }
+        if(flag==1)
+        {
+            cout<<x<<" "<<y<<endl;
+        }else
+            cout<<"No Solution"<<endl;
     }
     return 0;
 }
