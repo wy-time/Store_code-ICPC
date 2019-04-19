0fda28096e6d61500527a692bfb83f75ae939214
wy-time <1142942494@qq.com>
2019-04-19 22:00:46 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,104 +1,69 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-int toop(vector<int> G[2020], int n)
-{
-    int i;
-    int deg[2020] = {0};
-    queue<int>qu;
-    wfor(i, 1, n + 1)
-    {
-        int len = G[i].size();
-        int j;
-        wfor(j, 0, len)
-        {
-            deg[G[i][j]]++;
-        }
-    }
-    int cnt = 0;
-    wfor(i, 1, n + 1)
-    {
-        if (deg[i] == 0)
-        {
-            cnt++;
-            qu.push(i);
-        }
-    }
-    while (!qu.empty())
-    {
-        int v = qu.front();
-        qu.pop();
-        int len = G[v].size();
-        wfor(i, 0, len)
-        {
-            deg[G[v][i]]--;
-            if (deg[G[v][i]] == 0)
-            {
-                qu.push(G[v][i]);
-                cnt++;
-            }
-        }
-    }
-    if (cnt == n)
-        return 1;
-    else
-        return 0;
-}
 int main()
 {
     std::ios::sync_with_stdio(false);
-#ifdef test
-    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-#endif
-#ifdef ubuntu
-    freopen("/home/time/debug/debug/in", "r", stdin);
-    freopen("/home/time/debug/debug/out", "w", stdout);
-#endif
-    int t;
-    cin >> t;
-    while (t--)
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
+    int x,y,z;
+    while(cin>>x>>y>>z)
     {
-        vector<int>G1[2020];
-        vector<int>G2[2020];
-        vector<int>G3[2020];
-        int n;
-        cin >> n;
-        int i, j;
-        wfor(i, 1, n + 1)
+        ll ans=0;
+        int G1[100][100]={{0}};
+        int G2[100][100]={{0}};
+        int G3[100][100]={{0}};
+        int i,j;
+        wfor(i,0,x)
         {
-            wfor(j, 1, n + 1)
+            wfor(j,0,y)
             {
-                char c;
-                cin >> c;
-                switch (c)
-                {
-                case 'P':
-                    G1[i].push_back(j);
-                    G2[i].push_back(j);
-                    G3[j].push_back(i);
-                    break;
-                case 'Q':
-                    G1[i].push_back(j);
-                    G2[j].push_back(i);
-                    G3[i].push_back(j);
-                }
+                cin>>G1[i][j];
+                ans+=G1[i][j];
             }
         }
-        if (toop(G1, n) && toop(G2, n) && toop(G3, n))
+        wfor(i,0,y)
         {
-            cout << "T" << endl;
-        } else
-            cout << "N" << endl;
+            wfor(j,0,z)
+            {
+                cin>>G2[i][j];
+                ans+=G2[i][j];
+            }
+        }
+        wfor(i,0,z)
+        {
+            wfor(j,0,x)
+            {
+                cin>>G3[i][j];
+                ans+=G3[i][j];
+            }
+        }
+        wfor(i,0,x)
+        {
+            ans-=G1[i][y-1]&G3[z-1][i];
+        }
+        wfor(i,0,y)
+        {
+            ans-=G1[0][i]&G2[i][z-1];
+        }
+        wfor(i,0,z)
+        {
+            ans-=G2[y-1][i]&G3[i][0];
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
