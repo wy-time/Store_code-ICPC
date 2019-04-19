d5dbf8179517b12992d070a1b70ac63e72cb5e1f
wy-time <1142942494@qq.com>
2019-04-19 17:16:03 +0800

    cu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,106 +1,104 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
+#include <queue> 
+#include <vector> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-void read(int &x) {
-    char ch = getchar(); x = 0;
-    for (; ch < '0' || ch > '9'; ch = getchar());
-    for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-}
-const int maxn = 1005;
-int vis[105];
-int ma[105][105];
-vector<int>G[105];
-int ans[105];
-int anscnt;
-int res = 0;
-void dfs(int beg, int aim, int step, int pos)
+// void read(int &x) {
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// }
+int toop(vector<int> G[2020],int n)
 {
-    int len = G[beg].size();
     int i;
-    if (aim == step)
+    int deg[2020]={0};
+    queue<int>qu;
+    wfor(i,1,n+1)
     {
-        res++;
-        return ;
-    }
-    wfor(i, pos, len)
-    {
-        int v = G[beg][i];
-        if (!vis[v])
+        int len=G[i].size();
+        int j;
+        wfor(j,0,len)
         {
-            int j;
-            int flag = 1;
-            wfor(j, 0, anscnt)
+            deg[G[i][j]]++;
+        }
+    }
+    int cnt=0;
+    wfor(i,1,n+1)
+    {
+        if(deg[i]==0)
+        {
+            cnt++;
+            qu.push(i);
+        }
+    }
+    while(!qu.empty())
+    {
+        int v=qu.front();
+        qu.pop();
+        int len=G[v].size();
+        wfor(i,0,len)
+        {
+            deg[G[v][i]]--;
+            if(deg[G[v][i]]==0)
             {
-                if (ma[ans[j]][v] == 0)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-            if (flag == 1)
-            {
-                ans[anscnt++] = v;
-                dfs(beg, aim, step + 1, i + 1);
-                anscnt--;
+                qu.push(G[v][i]);
+                cnt++;
             }
         }
     }
+    if(cnt==n)
+        return 1;
+    else
+        return 0;
 }
 int main()
 {
-    // std::ios::sync_with_stdio(false);
-    // #ifdef test
-    // freopen("F:\\Desktop\\question\\in.txt","r",stdin);
-    // #endif
-    // #ifdef ubuntu
-    // freopen("/home/time/debug/debug/in","r",stdin);
-    // freopen("/home/time/debug/debug/out","w",stdout);
-    // #endif
+    std::ios::sync_with_stdio(false);
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
     int t;
-    // cin>>t;
-    read(t);
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-        memset(ma, 0, sizeof(ma));
-        memset(vis, 0, sizeof(vis));
-        res = 0;
-        int n, m, s;
-        // cin >> n >> m >> s;
-        read(n);
-        read(m);
-        read(s);
-        int i;
-        wfor(i, 1, 105)
-        G[i].clear();
-        wfor(i, 0, m)
+        vector<int>G1[2020];
+        vector<int>G2[2020];
+        vector<int>G3[2020];
+        int n;
+        cin>>n;
+        int i,j;
+        wfor(i,1,n+1)
         {
-            int u, v;
-            // cin >> u >> v;
-            read(u);
-            read(v);
-            G[u].push_back(v);
-            G[v].push_back(u);
-            ma[u][v] = 1;
-            ma[v][u] = 1;
-        }
-        wfor(i, 1, n + 1)
-        {
-            anscnt = 0;
-            if (G[i].size() < s - 1)
+            wfor(j,1,n+1)
             {
-                vis[i] = 1;
-                continue;
+                char c;
+                cin>>c;
+                switch (c)
+                {
+                    case 'P':
+                        G1[i].push_back(j);
+                        G2[i].push_back(j);
+                        G3[j].push_back(i);
+                        break;
+                    case 'Q':
+                        G1[i].push_back(j);
+                        G2[j].push_back(i);
+                        G3[i].push_back(j);
+                }
             }
-            dfs(i, s - 1, 0, 0);
-            vis[i] = 1;
         }
-        // cout << res << endl;
-        printf("%d\n", res);
+        if(toop(G1,n)&&toop(G2,n)&&toop(G3,n))
+        {
+            cout<<"T"<<endl;
+        }else
+            cout<<"N"<<endl;
     }
     return 0;
 }
