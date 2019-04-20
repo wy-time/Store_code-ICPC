f7adc1066c1b28edbfd9339a72255d9bd913a067
wy-time <1142942494@qq.com>
2019-04-20 20:09:26 +0800

    cu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,112 +1,124 @@
 #include <iostream>
-#include <cstring> 
+#include <cstring>
+#include <string> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn = 1e5 + 5;
-int num[maxn];
-int sum1[4][maxn];
+int ma[2][10005];
+const int mod=1e9+7;
+int slove (string s,ll temp)
+{
+    int i;
+    int len=s.size();
+    int num;
+    wfor(i,1,len)
+    {
+        num=s[i]-'0';
+        int have=ma[0][i]+ma[0][i-1]+ma[1][i]+ma[1][i-1];
+        int need=num-have;
+        if(need<0||2<need||(i==len-1&&need!=0))
+        {
+            temp=0;
+            break;
+        }
+        if(need==1)
+        {
+            ma[0][i+1]=1;
+            temp*=2;
+            temp%=mod;
+        }else if(need==2)
+        {
+            ma[0][i+1]=1;
+            ma[1][i+1]=1;
+        }
+    }
+    memset(ma,0,sizeof(ma));
+    return temp;
+}
 int main()
 {
-    // std::ios::sync_with_stdio(false);
-    // #ifdef test
-    // freopen("F:\\Desktop\\question\\in.txt","r",stdin);
-    // #endif
+    std::ios::sync_with_stdio(false);
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
     #ifdef ubuntu
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
     int t;
-    // cin>>t;
-    scanf("%d", &t);
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-        memset(sum1,0,sizeof(sum1));
-        sum1[0][0]=sum1[1][0]=sum1[2][0]=sum1[3][0]=0;
-        int n;
-        scanf("%d", &n);
-        // cin >> n;
-        int i;
-        wfor(i, 1, n + 1)
+        memset(ma,0,sizeof(ma));
+        string s;
+        cin>>s;
+        ll ans=0;
+        if(s[0]-'0'<=2)
         {
-            scanf("%d", &num[i]);
-            // cin >> num[i];
-        }
-        sum1[0][1]=num[1];
-        for(i=5;i<=n;i+=4)
-        {
-            sum1[0][i]^=sum1[0][i-4]^num[i];
-        }
-        sum1[1][2]=num[2];
-        for(i=6;i<=n;i+=4)
-        {
-            sum1[1][i]^=sum1[1][i-4]^num[i];
-        }
-        sum1[2][3]=num[3];
-        for(i=7;i<=n;i+=4)
-        {
-            sum1[2][i]^=sum1[2][i-4]^num[i];
-        }
-        sum1[3][4]=num[4];
-        for(i=8;i<=n;i+=4)
-        {
-            sum1[3][i]^=sum1[3][i-4]^num[i];
-        }
-        int q;
-        // cin >> q;
-        scanf("%d", &q);
-        while (q--)
-        {
-            ll ans = 0;
-            int l, r;
-            //cin >> l >> r;
-            scanf("%d %d", &l, &r);
-            int len = r - l + 1;
-            if (len % 4 == 0)
+            int num=s[0]-'0';
+            ll temp=1;
+            if(num==1)
             {
-                ans = 0;
-            } else if (len % 4 == 1)
+                ma[0][1]=1;
+                temp=2;
+            }else if(num==2)
             {
-                int pos=l%4;
-                if(pos==0)
-                    pos=3;
-                else
-                    pos--;
-                ans=sum1[pos][max(0,l-4)]^sum1[pos][(r-1-pos)/4*4+1+pos];
-            } else if (len % 4 == 2)
-            {
-                int pos=l%4;
-                if(pos==0)
-                    pos=3;
-                else
-                    pos--;
-                ans=sum1[pos][max(0,l-4)]^sum1[pos][(r-1-pos)/4*4+1+pos];
-                pos=(l+1)%4;
-                if(pos==0)
-                    pos=3;
-                else
-                    pos--;
-                ans^=sum1[pos][max(l-3,0)]^sum1[pos][(r-1-pos)/4*4+1+pos];
-            } else
-            {
-                int pos=(l+1)%4;
-                if(pos==0)
-                    pos=3;
-                else
-                    pos--;
-                ans=sum1[pos][max(0,l-3)]^sum1[pos][(r-1-pos)/4*4+1+pos];
+                temp=1;
+                ma[0][1]=1;
+                ma[1][1]=1;
             }
-            // cout << ans << endl;
-            printf("%lld\n", ans);
+            ans+=slove(s,temp);
         }
-        cout<<endl;
+        if(s[0]-'0'<=3&&s[0]-'0'>=1)
+        {
+            int num=s[0]-'0';
+            ll temp=1;
+            if(num==3)
+            {
+                ma[0][1]=1;
+                ma[1][1]=1;
+                ma[0][0]=1;
+                temp=2;
+            }else if(num==2)
+            {
+                ma[0][0]=1;
+                ma[0][1]=1;
+                temp=4;
+            }else if(num==1)
+            {
+                ma[0][0]=1;
+                temp=2;
+            }
+            ans+=slove(s,temp);
+        }
+        if(s[0]-'0'<=4&&s[0]-'0'>=2)
+        {
+            int num=s[0]-'0';
+            ll temp=1;
+            ma[0][0]=1;
+            ma[1][0]=1;
+            if(num==2)
+            {
+                temp=1;
+            }else if(num==3)
+            {
+                ma[0][1]=1;
+                temp=2;
+            }else if(num==4)
+            {
+                ma[0][1]=ma[1][1]=1;
+                temp=1;
+            }
+            ans+=slove(s,temp);
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
