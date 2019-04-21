e0329bb2cddde1932ff3ef1c9349217e55a9df4c
wy-time <1142942494@qq.com>
2019-04-21 20:15:08 +0800

    poj-3468

--- debug/main.cpp
+++ debug/main.cpp
@@ -9,51 +9,66 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=200005;
+const int maxn=100005;
 int num[maxn];
-int tree[maxn<<2];
-void push_up(int now)
+ll tree[maxn<<2];
+ll add[maxn<<2];
+void push_up(int id)
 {
-    tree[now]=max(tree[now<<1],tree[now<<1|1]);
+    tree[id]=tree[id<<1]+tree[id<<1|1];
 }
-void build(int l,int r,int now)
-{
-    if(l==r)
-    {
-        tree[now]=num[l];
-        return ;
-    }
-    int mid=(l+r)>>1;
-    build(l,mid,now<<1);
-    build(mid+1,r,now<<1|1);
-    push_up(now);
-}
-void update(int l,int r,int now,int id,int number)
+void build(int l,int r,int id)
 {
     if(l==r)
     {
-        tree[now]=number;
+        tree[id]=num[l];
         return ;
     }
     int mid=(l+r)>>1;
-    if(id<=mid)
-        update(l,mid,now<<1,id,number);
-    else
-        update(mid+1,r,now<<1|1,id,number);
-    push_up(now);
+    build(l,mid,id<<1);
+    build(mid+1,r,id<<1|1);
+    push_up(id);
 }
-int query(int l,int r,int now,int L,int R)
+void push_down(int id,int ln,int rn)
+{
+    if(add[id]!=0)
+    {
+        tree[id<<1]+=add[id]*(ll)ln;
+        tree[id<<1|1]+=add[id]*(ll)rn;
+        add[id<<1]+=add[id];
+        add[id<<1|1]+=add[id];
+        add[id]=0;
+    }
+}
+void update(int l,int r,int L,int R,int id,ll number)
 {
     if(l>=L&&r<=R)
     {
-        return tree[now];
+        tree[id]+=number*(ll)(r-l+1);
+        add[id]+=number;
+        return ;
     }
-    int ans=0;
     int mid=(l+r)>>1;
+    push_down(id,mid-l+1,r-mid);
     if(mid>=L)
-        ans=max(ans,query(l,mid,now<<1,L,R));
+        update(l,mid,L,R,id<<1,number);
     if(mid<R)
-        ans=max(ans,query(mid+1,r,now<<1|1,L,R));
+        update(mid+1,r,L,R,id<<1|1,number);
+    push_up(id);
+}
+ll query(int l,int r,int L,int R,int id)
+{
+    if(l>=L&&r<=R)
+    {
+        return tree[id];
+    }
+    ll ans=0;
+    int mid=(l+r)>>1;
+    push_down(id,mid-l+1,r-mid);
+    if(mid>=L)
+        ans+=query(l,mid,L,R,id<<1);
+    if(mid<R)
+        ans+=query(mid+1,r,L,R,id<<1|1);
     return ans;
 }
 int main()
@@ -66,31 +81,30 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n,m;
-    while(cin>>n>>m)
+    int n,q;
+    cin>>n>>q;
+    int i;
+    wfor(i,1,n+1)
     {
-        int i;
-        wfor(i,1,n+1)
+        cin>>num[i];
+    }
+    build(1,n,1);
+    wfor(i,0,q)
+    {
+        char op;
+        cin>>op;
+        if(op=='Q')
         {
-            cin>>num[i];
-        }
-        build(1,n,1);
-        wfor(i,0,m)
+            int l,r;
+            cin>>l>>r;
+            ll ans=query(1,n,l,r,1);
+            cout<<ans<<endl;
+        }else
         {
-            char op;
-            cin>>op;
-            if(op=='Q')
-            {
-                int l,r;
-                cin>>l>>r;
-                int ans=query(1,n,1,l,r);
-                cout<<ans<<endl;
-            }else
-            {
-                int id,number;
-                cin>>id>>number;
-                update(1,n,1,id,number);
-            }
+            int l,r;
+            ll number;
+            cin>>l>>r>>number;
+            update(1,n,l,r,1,number);
         }
     }
     return 0;
