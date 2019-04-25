6d6d0211b5a5235d31fcfda33a8821ede936805e
wy-time <1142942494@qq.com>
2019-04-25 18:49:05 +0800

    zoj-1610

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm> 
+#include <map> 
 #include <cstring> 
 #include <cstdio>
 using namespace std;
@@ -10,31 +12,14 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=100005;
+const int maxn =8005;
 int tree[maxn<<2];
 int target[maxn<<2];
-void push_up(int id)
-{
-    tree[id]=tree[id<<1]+tree[id<<1|1];
-}
-void bulid(int l,int r,int id)
-{
-    if(l==r)
-    {
-        tree[id]=1;
-        return ;
-    }
-    int mid=(l+r)>>1;
-    bulid(l,mid,id<<1);
-    bulid(mid+1,r,id<<1|1);
-    push_up(id);
-}
-void push_down(int ln,int rn,int id)
+void push_down(int id)
 {
     if(target[id])
     {
-        tree[id<<1]=ln*target[id];
-        tree[id<<1|1]=rn*target[id];
+        tree[id<<1]=tree[id<<1|1]=target[id];
         target[id<<1]=target[id<<1|1]=target[id];
         target[id]=0;
     }
@@ -43,17 +28,31 @@ void updata(int l,int r,int L,int R,int id,int number)
 {
     if(l>=L&&r<=R)
     {
-        tree[id]=(r-l+1)*number;
+        tree[id]=number;
         target[id]=number;
         return ;
     }
+    push_down(id);
     int mid=(l+r)>>1;
-    push_down(mid-l+1,r-mid,id);
     if(mid>=L)
         updata(l,mid,L,R,id<<1,number);
     if(mid<R)
         updata(mid+1,r,L,R,id<<1|1,number);
-    push_up(id);
+}
+int query(int l,int r,int id,int pos)
+{
+    if(l==r)
+    {
+        return tree[id];
+    }
+    push_down(id);
+    int ans=0;
+    int mid=(l+r)>>1;
+    if(mid>=pos)
+        ans=query(l,mid,id<<1,pos);
+    else
+        ans=query(mid+1,r,id<<1|1,pos);
+    return ans;
 }
 int main()
 {
@@ -65,28 +64,46 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int t;
-    cin>>t;
-    int casecnt=0;
-    while(t--)
+    int n;
+    while(cin>>n)
     {
-        casecnt++;
-        int n;
-        cin>>n;
-        memset(target,0,sizeof(target));
-        bulid(1,n,1);
-        int q;
-        cin>>q;
+        memset(tree,0,sizeof(tree));
         int i;
-        wfor(i,0,q)
+        map<int,int>ma;
+        int maxnum=0;
+        wfor(i,0,n)
         {
-            int l,r,z;
-            cin>>l>>r>>z;
-            updata(1,n,l,r,1,z);
+            int l,r,id;
+            cin>>l>>r>>id;
+            l++;
+            id++;
+            maxnum=max(maxnum,r);
+            updata(1,8002,l,r,1,id);
         }
-        int ans=tree[1];
-        cout<<"Case "<<casecnt<<": The total value of the hook is ";
-        cout<<ans<<"."<<endl;
+        int last=0;
+        wfor(i,1,maxnum+1)
+        {
+            int tem=query(1,8002,1,i);
+            if(tem==0)
+            {
+                last=0;
+                continue;
+            }
+            if(tem!=last)
+            {
+                if(ma.count(tem-1)==0)
+                    ma.insert(make_pair(tem-1,1));
+                else
+                    ma[tem-1]++;
+                last=tem;
+            }
+        }
+        map<int,int>::iterator it;
+        for(it=ma.begin();it!=ma.end();it++)
+        {
+            cout<<it->first<<" "<<it->second<<endl;
+        }
+        cout<<endl;
     }
     return 0;
 }
