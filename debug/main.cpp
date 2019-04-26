78bae530f5d6bba99121edfe0d8d2cb20332986a
wy-time <1142942494@qq.com>
2019-04-26 18:52:22 +0800

    hdu-4027

--- debug/main.cpp
+++ debug/main.cpp
@@ -10,19 +10,62 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-int bit[33];
-void init()
+const int maxn=1e5+5;
+ll tree[maxn<<2];
+ll maxnum[maxn<<2];
+ll num[maxn];
+void push_up(int id)
 {
-    int i;
-    wfor(i,0,32)
-    {
-        bit[i]=1<<i;
-    }
+    maxnum[id]=max(maxnum[id<<1],maxnum[id<<1|1]);
+    tree[id]=tree[id<<1]+tree[id<<1|1];
 }
-const int maxn=5e3+5;
-int num[maxn];
-int maxnum[maxn][30];
-int minnum[maxn][30];
+void bulid(int l,int r,int id)
+{
+    if(l==r)
+    {
+        tree[id]=num[l];
+        maxnum[id]=num[l];
+        return ;
+    }
+    int mid=(l+r)>>1;
+    bulid(l,mid,id<<1);
+    bulid(mid+1,r,id<<1|1);
+    push_up(id);
+}
+void updata(int l,int r,int L,int R,int id)
+{
+    if(maxnum[id]<=1)
+    {
+        return ;
+    }
+    if(l==r)
+    {
+        tree[id]=sqrt(tree[id]);
+        maxnum[id]=tree[id];
+        return ;
+    }
+    int mid=(l+r)>>1;
+    if(mid>=L)
+        updata(l,mid,L,R,id<<1);
+    push_up(id);
+    if(mid<R)
+        updata(mid+1,r,L,R,id<<1|1);
+    push_up(id);
+}
+ll query(int l,int r,int L,int R,int id)
+{
+    if(l>=L&&r<=R)
+    {
+        return tree[id];
+    }
+    ll ans=0;
+    int mid=(l+r)>>1;
+    if(mid>=L)
+        ans+=query(l,mid,L,R,id<<1);
+    if(mid<R)
+        ans+=query(mid+1,r,L,R,id<<1|1);
+    return ans;
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -33,41 +76,33 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n,q;
-    cin>>n>>q;
-    int i;
-    init();
-    wfor(i,1,n+1)
+    int n;
+    int casecnt=0;
+    while(cin>>n)
     {
-        cin>>num[i];
-    }
-    int j;
-    int en=log2(n);
-    wfor(i,1,n+1)
-    {
-        maxnum[i][0]=num[i];
-        minnum[i][0]=num[i];
-    }
-    wfor(j,1,en+2)
-    {
+        casecnt++;
+        cout<<"Case #"<<casecnt<<":"<<endl;
+        int i;
         wfor(i,1,n+1)
         {
-            if(i+bit[j-1]<=n)
+            cin>>num[i];
+        }
+        bulid(1,n,1);
+        int m;
+        cin>>m;
+        wfor(i,0,m)
+        {
+            int t,l,r;
+            cin>>t>>l>>r;
+            if(t==0)
             {
-                maxnum[i][j]=max(maxnum[i][j-1],maxnum[i+bit[j-1]][j-1]);
-                minnum[i][j]=min(minnum[i][j-1],minnum[i+bit[j-1]][j-1]);
+                updata(1,n,l,r,1);
+            }else
+            {
+                int ans=query(1,n,l,r,1);
+                cout<<ans<<endl;
             }
         }
-    }
-    wfor(i,0,q)
-    {
-        int l,r;
-        cin>>l>>r;
-        int len=r-l+1;
-        int t=log2(len);
-        int x=max(maxnum[l][t],maxnum[r-bit[t]+1][t]);
-        int y=min(minnum[l][t],minnum[r-bit[t]+1][t]);
-        cout<<x-y<<endl;
     }
     return 0;
 }
