b31cb90461f78ec68c862bbae161e4519eb0047b
wy-time <1142942494@qq.com>
2019-04-29 22:18:39 +0800

    hdu-4553

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,215 +1,241 @@
 #include <iostream>
-#include <cstring>
+#include <cstring> 
+#include <string> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn = 5e4 + 5;
+const int maxn=100005;
 struct node
 {
-    int sum;
-    int maxnum;
-    int minnum;
-    int change;
+    int len;
+    int llen;
+    int rlen;
+    int l;
+    int r;
+    int left;
+    int right;
+    int isall;
+    int chan;
 };
-node tree[maxn << 2];
-int n;
-void push_up(int id)
+node tree1[maxn<<2];
+node tree2[maxn<<2];
+void push_up(int id,node *tree)
 {
-    tree[id].sum = tree[id << 1].sum + tree[id << 1 | 1].sum;
-    tree[id].maxnum = max(tree[id << 1].maxnum, tree[id << 1 | 1].maxnum);
-    tree[id].minnum = min(tree[id << 1].minnum, tree[id << 1 | 1].minnum);
-}
-void build(int l, int r, int id)
-{
-    if (l == r)
+    tree[id].len=max(tree[id<<1].len,max(tree[id<<1|1].len,tree[id<<1].rlen+tree[id<<1|1].llen));
+    if(tree[id].len==tree[id<<1].len)
     {
-        tree[id].sum = 1;
-        tree[id].maxnum = l;
-        tree[id].minnum = l;
-        tree[id].change = 0;
+        tree[id].l=tree[id<<1].l;
+        tree[id].r=tree[id<<1].r;
+    }else if(tree[id].len==tree[id<<1|1].len)
+    {
+        tree[id].l=tree[id<<1|1].l;
+        tree[id].r=tree[id<<1|1].r;
+    }else
+    {
+        tree[id].l=tree[id<<1].right;
+        tree[id].r=tree[id<<1|1].left;
+    }
+    if(tree[id<<1].isall==0)
+    {
+        tree[id].llen=tree[id<<1].llen;
+        tree[id].left=tree[id<<1].left;
+    }else
+    {
+        tree[id].llen=tree[id<<1].llen+tree[id<<1|1].llen;
+        tree[id].left=tree[id<<1|1].left;
+    }
+
+    if(tree[id<<1|1].isall==0)
+    {
+        tree[id].rlen=tree[id<<1|1].rlen;
+        tree[id].right=tree[id<<1|1].right;
+    }else
+    {
+        tree[id].rlen=tree[id<<1|1].rlen+tree[id<<1].rlen;
+        tree[id].right=tree[id<<1].right;
+    }
+    if(tree[id].llen==tree[id].len)
+        tree[id].isall=1;
+    else
+        tree[id].isall=0;
+}
+void init(int id,node *tree,int l)
+{
+    tree[id].isall=1;
+    tree[id].chan=0;
+    tree[id].l=tree[id].r=l;
+    tree[id].right=tree[id].left=l;
+    tree[id].llen=tree[id].rlen=tree[id].len=1;
+}
+void build(int l,int r,int id)
+{
+    if(l==r)
+    {
+        init(id,tree1,l);
+        init(id,tree2,l);
         return ;
     }
-    int mid = (l + r) >> 1;
-    build(l, mid, id << 1);
-    build(mid + 1, r, id << 1 | 1);
-    push_up(id);
+    int mid=(l+r)>>1;
+    build(l,mid,id<<1);
+    build(mid+1,r,id<<1|1);
+    push_up(id,tree1);
+    push_up(id,tree2);
 }
-void push_down(int l, int r, int id)
+void down_proc(int id,node*tree)
 {
-    if (tree[id].change == 1)
+    tree[id].len=tree[id].llen=tree[id].rlen=0;
+    tree[id].l=tree[id].r=tree[id].left=tree[id].right=-1;
+    tree[id].isall=0;
+    tree[id].chan=1;
+}
+void time_proc(int l,int r,int id,node*tree)
+{
+    int len=r-l+1;
+    tree[id].len=tree[id].llen=tree[id].rlen=len;
+    tree[id].r=tree[id].left=r;
+    tree[id].l=tree[id].right=l;
+    tree[id].isall=1;
+    tree[id].chan=2;
+}
+void push_down(int l,int r,int id,node*tree)
+{
+    if(tree[id].chan==1)
     {
-        int mid = (l + r) >> 1;
-        tree[id << 1].sum = mid - l + 1;
-        tree[id << 1 | 1].sum = r - mid;
-        tree[id << 1].maxnum = max(mid, tree[id << 1].maxnum);
-        tree[id << 1].minnum = min(l, tree[id << 1].minnum);
-        tree[id << 1 | 1].maxnum = max(tree[id << 1 | 1].maxnum, r);
-        tree[id << 1 | 1].minnum = min(tree[id << 1 | 1].minnum, mid + 1);
-        tree[id << 1].change = tree[id << 1 | 1].change = 1;
-        tree[id].change = 0;
+        down_proc(id<<1,tree);
+        down_proc(id<<1|1,tree);
+        tree[id].chan=0;
     }
-    if (tree[id].change == 2)
+    if(tree[id].chan==2)
     {
-        tree[id << 1].sum = 0;
-        tree[id << 1 | 1].sum = 0;
-        tree[id << 1].maxnum = -2;
-        tree[id << 1].minnum = 1e9;
-        tree[id << 1 | 1].maxnum = -2;
-        tree[id << 1 | 1].minnum = 1e9;
-        tree[id << 1].change = tree[id << 1 | 1].change = 2;
-        tree[id].change = 0;
+        int mid=(l+r)>>1;
+        time_proc(l,mid,id<<1,tree);
+        time_proc(mid+1,r,id<<1|1,tree);
+        tree[id].chan=0;
     }
 }
-void updata_clean(int l, int r, int L, int R, int id)
+void updata(int l,int r,int L,int R,int id,node*tree)
 {
-    if (l >= L && r <= R)
+    if(l>=L&&r<=R)
     {
-        tree[id].sum = r - l + 1;
-        tree[id].maxnum = max(r, tree[id].maxnum);
-        tree[id].minnum = min(l, tree[id].minnum);
-        tree[id].change = 1;
-        return ;
-    }
-    int mid = (l + r) >> 1;
-    push_down(l, r, id);
-    if (mid >= L)
-        updata_clean(l, mid, L, R, id << 1);
-    if (mid < R)
-        updata_clean(mid + 1, r, L, R, id << 1 | 1);
-    push_up(id);
-}
-int query_disc(int l, int r, int L, int R, int id)
-{
-    if (l >= L && r <= R)
-    {
-        return tree[id].sum;
-    }
-    int mid = (l + r) >> 1;
-    push_down(l, r, id);
-    int ans = 0;
-    if (mid >= L)
-        ans += query_disc(l, mid, L, R, id << 1);
-    if (mid < R)
-        ans += query_disc(mid + 1, r, L, R, id << 1 | 1);
-    return ans;
-}
-void updata_use(int l, int r, int L, int R, int id)
-{
-    if (l >= L && r <= R)
-    {
-        tree[id].sum = 0;
-        tree[id].maxnum = -2;
-        tree[id].minnum = 1e9;
-        tree[id].change = 2;
+        down_proc(id,tree);
         return;
     }
-    int mid = (l + r) >> 1;
-    push_down(l, r, id);
-    if (mid >= L)
-        updata_use(l, mid, L, R, id << 1);
-    if (mid < R)
-        updata_use(mid + 1, r, L, R, id << 1 | 1);
-    push_up(id);
+    int mid=(l+r)>>1;
+    push_down(l,r,id,tree);
+    if(mid>=L)
+        updata(l,mid,L,R,id<<1,tree);
+    if(mid<R)
+        updata(mid+1,r,L,R,id<<1|1,tree);
+    push_up(id,tree);
 }
-int x, y;
-void query_in(int l, int r, int L, int R, int id, int &number)
+void study(int l,int r,int L,int R,int id,node*tree)
 {
-    if (number == 0)
+    if(l>=L&&r<=R)
+    {
+        time_proc(l,r,id,tree);
         return ;
-    if (l >= L && r <= R)
-    {
-        int have = tree[id].sum;
-        if (have <= 0)
-            return ;
-        if (have <= number)
-        {
-            x = min(x, tree[id].minnum);
-            y = max(y, tree[id].maxnum);
-            updata_use(1, n, x, y, 1);
-            number -= have;
-            return ;
-        }
     }
-    if (l == r)
+    int mid=(l+r)>>1;
+    push_down(l,r,id,tree);
+    if(mid>=L)
+        study(l,mid,L,R,id<<1,tree);
+    if(mid>R)
+        study(mid+1,r,L,R,id<<1|1,tree);
+    push_up(id,tree);
+}
+void find_time(int l,int r,int id,int len,int &x,node *tree)
+{
+    if(tree[id].len<len)
     {
-        if (tree[id].sum == 1)
-        {
-            number--;
-            x = min(x, l);
-            y = max(y, l);
-            tree[id].sum = 0;
-            tree[id].maxnum = -2;
-            tree[id].minnum = 1e9;
-            return ;
-        }
+        return ;
+    }else
+    {
+        x=min(x,tree[id].l);
     }
-    int mid = (l + r) >> 1;
-    push_down(l, r, id);
-    if (mid >= L)
-        query_in(l, mid, L, R, id << 1, number);
-    if (mid < R)
-        query_in(mid + 1, r, L, R, id << 1 | 1, number);
+    int mid=(l+r)>>1;
+    push_down(l,r,id,tree);
+    find_time(l,mid,id<<1,len,x,tree);
+    find_time(mid+1,r,id<<1|1,len,x,tree);
 }
 int main()
 {
     std::ios::sync_with_stdio(false);
-#ifdef test
-    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-    freopen("F:\\Desktop\\question\\out.txt", "w", stdout);
-#endif
-#ifdef ubuntu
-    freopen("/home/time/debug/debug/in", "r", stdin);
-    freopen("/home/time/debug/debug/out", "w", stdout);
-#endif
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif
     int t;
-    cin >> t;
-    while (t--)
+    int casecnt=0;
+    cin>>t;
+    while(t--)
     {
-        memset(tree, 0, sizeof(tree));
-        int m;
-        cin >> n >> m;
-        build(1, n, 1);
+        casecnt++;
+        int n,m;
+        cin>>n>>m;
+        memset(tree1,0,sizeof(tree1));
+        memset(tree2,0,sizeof(tree2));
+        build(1,n,1);
         int i;
-        wfor(i, 0, m)
+        cout<<"Case "<<casecnt<<":"<<endl;
+        wfor(i,0,m)
         {
-            int op;
-            cin >> op;
-            if (op == 1)
+            string s;
+            int tim;
+            cin>>s>>tim;
+            if(s[0]=='D')
             {
-                int pos, number;
-                cin >> pos >> number;
-                x = 1e9;
-                y = -1;
-                pos++;
-                query_in(1, n, pos, n, 1, number);
-                if (y == -1)
+                int x=1e9;
+                find_time(1,n,1,tim,x,tree1);
+                if(x==1e9)
                 {
-                    cout << "Can not put any one." << endl;
-                } else
+                    cout<<"fly with yourself"<<endl;
+                }else
                 {
-                    x--;
-                    y--;
-                    cout << x << " " << y << endl;
+                    cout<<x<<",let's fly"<<endl;
+                    updata(1,n,x,x+tim-1,1,tree1);
                 }
-            } else
+            }else if(s[0]=='N')
             {
-                int l, r;
-                cin >> l >> r;
-                l++;
-                r++;
-                int ans = query_disc(1, n, l, r, 1);
-                cout << r - l + 1 - ans << endl;
-                updata_clean(1, n, l, r, 1);
+                int x=1e9;
+                find_time(1,n,1,tim,x,tree1);
+                if(x==1e9)
+                {
+                    find_time(1,n,1,tim,x,tree2);
+                    if(x==1e9)
+                    {
+                        cout<<"wait for me"<<endl;
+                    }else
+                    {
+                        cout<<x<<",don't put my gezi"<<endl;
+                        updata(1,n,x,x+tim-1,1,tree1);
+                        updata(1,n,x,x+tim-1,1,tree2);
+                    }
+                }else
+                {
+                    cout<<x<<",don't put my gezi"<<endl;
+                    updata(1,n,x,x+tim-1,1,tree1);
+                    updata(1,n,x,x+tim-1,1,tree2);
+                }
+            }else
+            {
+                int l,r;
+                l=tim;
+                cin>>r;
+                study(1,n,l,r,1,tree1);
+                study(1,n,l,r,1,tree2);
+                cout<<"I am the hope of chinese chengxuyuan!!"<<endl;
             }
         }
-        cout << endl;
     }
     return 0;
 }
