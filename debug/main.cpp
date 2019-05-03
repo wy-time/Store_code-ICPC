57798355958402e844dd668955ffd3f54a5e74bd
wy-time <1142942494@qq.com>
2019-05-03 16:40:03 +0800

    cf-1154-E

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue> 
+#include <algorithm> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -10,7 +12,19 @@ typedef long long ll;
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
 const int maxn=2e5+5;
-int num[maxn];
+int ans[maxn];
+struct st
+{
+    int num;
+    int id;
+};
+struct rule 
+{
+    bool operator()(const st &a,const st&b)
+    {
+        return a.num<b.num;
+    }
+};
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -21,44 +35,62 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n,a,b;
-    cin>>n>>b>>a;
+    int n,k;
+    cin>>n>>k;
     int i;
+    priority_queue<st,vector<st>,rule>qu;
+    st num;
     wfor(i,0,n)
     {
-        cin>>num[i];
+        cin>>num.num;
+        num.id=i;
+        qu.push(num);
     }
-    int ans=0;
-    int maxa=a;
+    int flag=1;
     wfor(i,0,n)
     {
-        if(a==0&&b==0)
+        if(qu.empty())
             break;
-        if(num[i]==1)
+        num=qu.top();
+        qu.pop();
+        while(ans[num.id]!=0&&!qu.empty())
         {
-            if(b!=0)
-            {
-                b--;
-                a=min(a+1,maxa);
-                ans++;
-            }else if(a!=0)
-            {
-                a--;
-                ans++;
-            }
-        }else 
-        {
-            if(a!=0)
-            {
-                a--;
-                ans++;
-            }else if(b!=0)
-            {
-                b--;
-                ans++;
-            }
+            num=qu.top();
+            qu.pop();
         }
+        int cnt=0;
+        int pos=num.id;
+        if(ans[pos]==0)
+            ans[pos]=flag;
+        while(cnt<k)
+        {
+            if(ans[pos]==0)
+            {
+                ans[pos]=flag;
+                cnt++;
+            }
+            pos++;
+            if(pos>=n)
+                break;
+        }
+        cnt=0;
+        pos=num.id;
+        while(cnt<k)
+        {
+            if(ans[pos]==0)
+            {
+                ans[pos]=flag;
+                cnt++;
+            }
+            pos--;
+            if(pos<0)
+                break;
+        }
+        flag=flag==1?2:1;
     }
-    cout<<ans<<endl;
+    wfor(i,0,n)
+    {
+        cout<<ans[i];
+    }
     return 0;
 }
