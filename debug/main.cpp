b90cc1245c203bdbed909a40ae7e39d6d20f60cd
wy-time <1142942494@qq.com>
2019-05-04 19:51:45 +0800

    nowcoder

--- debug/main.cpp
+++ debug/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(int &x) {
+// void read(ll &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
@@ -21,14 +21,14 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int t;
+    ll t;
     cin>>t;
     while(t--)
     {
-        int l,r;
+        ll l,r;
         cin>>l>>r;
         ll one=l-1;
-        int beg=0;
+        ll beg=0;
         if(one%192==0)
         {
             beg=one/192;
@@ -37,9 +37,10 @@ int main()
         {
             beg=one/192+1;
             one=(one/192+1)*192;
+            one++;
         }
         ll last=r-1;
-        int end=0;
+        ll end=0;
         if(last%192==0)
         {
             end=last/192;
@@ -48,6 +49,7 @@ int main()
         {
             end=last/192;
             last=last/192*192;
+            last++;
         }
         ll len=end-beg+1;
         ll sum=len*one+(len)*(len-1)*96;
