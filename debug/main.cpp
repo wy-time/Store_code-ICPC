a30453cc46ba43f0c646496ba237b0704a544f3f
wy-time <1142942494@qq.com>
2019-05-04 20:02:12 +0800

    nowcoder

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
-#include <algorithm> 
-#include <cmath> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(ll &x) {
+// void read(int &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+struct st
+{
+    int num;
+    int tim;
+    int last;
+    bool operator <(const st&x)const
+    {
+        if(x.num!=num)
+        {
+            return num>x.num;
+        }else if(tim!=x.tim)
+            return tim<x.tim;
+        else 
+            return last<x.last;
+    }
+    bool operator ==(const st&x)const
+    {
+        if(x.num==num&&x.tim==tim&&x.last==last)
+        {
+            return true;
+        }else
+            return false;
+    }
+};
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -21,39 +43,16 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    ll t;
-    cin>>t;
-    while(t--)
+    st a,b;
+    cin>>a.num>>a.tim>>a.last;
+    cin>>b.num>>b.tim>>b.last;
+    if(a==b)
     {
-        ll l,r;
-        cin>>l>>r;
-        ll one=l-1;
-        ll beg=0;
-        if(one%192==0)
-        {
-            beg=one/192;
-            one=1+one;
-        }else
-        {
-            beg=one/192+1;
-            one=(one/192+1)*192;
-            one++;
-        }
-        ll last=r-1;
-        ll end=0;
-        if(last%192==0)
-        {
-            end=last/192;
-            last+=1;
-        }else
-        {
-            end=last/192;
-            last=last/192*192;
-            last++;
-        }
-        ll len=end-beg+1;
-        ll sum=len*one+(len)*(len-1)*96;
-        cout<<sum<<endl;
-    }
+        cout<<"God"<<endl;
+    }else if(a<b)
+    {
+        cout<<1<<endl;
+    }else 
+        cout<<2<<endl;
     return 0;
 }
