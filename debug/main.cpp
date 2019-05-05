40bf5a97ab088c2d247d487f8876c7a28d450c85
wy-time <1142942494@qq.com>
2019-05-05 17:05:57 +0800

    vj

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <algorithm> 
+#include <queue> 
+#include <string> 
+#include <cstring> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -12,18 +14,98 @@ typedef long long ll;
 // }
 struct st
 {
-    int a;
-    int b;
-};
-struct rule
-{
-    bool operator ()(const st &x,const st&y)
+    char s;
+    int next[26];
+    int fail;
+    int is_last;
+    void init()
     {
-        return x.a-x.b>y.a-y.b;
+        memset(next,-1,sizeof(next));
+        fail=-1;
+        is_last=0;
     }
 };
-const int maxn=1e5+5;
-st num[maxn];
+const int maxn=500005;
+st trie[maxn];
+int cnt=0;
+void build(string s)
+{
+    int len=s.size();
+    int now=0;
+    int i;
+    wfor(i,0,len)
+    {
+        int id=s[i]-'a';
+        if(trie[now].next[id]==-1)
+        {
+            trie[++cnt].init();
+            trie[cnt].s=s[i];
+            trie[now].next[id]=cnt;
+        }
+        now=trie[now].next[id];
+    }
+    trie[now].is_last++;
+}
+void cla_fail()
+{
+    int i;
+    queue<int>qu;
+    wfor(i,0,26)
+    {
+        if(trie[0].next[i]!=-1)
+        {
+            int p=trie[0].next[i];
+            trie[p].fail=0;
+            qu.push(p);
+        }
+    }
+    while(!qu.empty())
+    {
+        int father=qu.front();
+        qu.pop();
+        wfor(i,0,26)
+        {
+            if(trie[father].next[i]!=-1)
+            {
+                int child=trie[father].next[i];
+                int p=trie[father].fail;
+                while(p!=-1&&trie[p].next[i]==-1)
+                    p=trie[p].fail;
+                if(p==-1)
+                    trie[child].fail=0;
+                else
+                    trie[child].fail=trie[p].next[i];
+                qu.push(child);
+            }
+        }
+    }
+}
+int query(string s)
+{
+    int len=s.size();
+    int i;
+    int now=0;
+    int ans=0;
+    wfor(i,0,len)
+    {
+        int id=s[i]-'a';
+        while(now!=0&&trie[now].next[id]==-1)
+            now=trie[now].fail;
+        int temp;
+        now=trie[now].next[id];
+        if(now!=-1)
+            temp=now;
+        else
+            temp=now=0;
+        while(temp!=0&&trie[temp].is_last!=-1)
+        {
+            ans+=trie[temp].is_last;
+            trie[temp].is_last=-1;
+            temp=trie[temp].fail;
+        }
+    }
+    return ans;
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -34,20 +116,24 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n;
-    cin>>n;
-    int i;
-    wfor(i,0,n)
+    int t;
+    cin>>t;
+    while(t--)
     {
-        cin>>num[i].a>>num[i].b;
+        trie[0].init();
+        int n;
+        cin>>n;
+        int i;
+        string s;
+        wfor(i,0,n)
+        {
+            cin>>s;
+            build(s);
+        }
+        cla_fail();
+        cin>>s;
+        int ans=query(s);
+        cout<<ans<<endl;
     }
-    sort(num,num+n,rule());
-    ll ans=0;
-    wfor(i,0,n)
-    {
-        ans+=num[i].a*i;
-        ans+=num[i].b*(n-i-1);
-    }
-    cout<<ans<<endl;
     return 0;
 }
