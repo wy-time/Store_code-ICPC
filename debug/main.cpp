44f7de5e3155757302c024617af3c1f93da36805
wy-time <1142942494@qq.com>
2019-05-05 21:00:43 +0800

    vj

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,108 +1,137 @@
 #include <iostream>
 #include <queue> 
-#include <string> 
 #include <cstring> 
+#include <string> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(int &x) {
+// void read(ll &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+const ll maxn=2e6+5;
+const ll mod=100000;
+ll ma[200];
 struct st
 {
-    char s;
-    int next[26];
-    int fail;
-    int is_last;
+    ll next[4];
+    ll fail;
+    ll is_string;
     void init()
     {
         memset(next,-1,sizeof(next));
         fail=-1;
-        is_last=0;
+        is_string=0;
     }
 };
-const int maxn=500005;
 st trie[maxn];
-int cnt=0;
+void init()
+{
+    ma['A']=0;
+    ma['C']=1;
+    ma['G']=2;
+    ma['T']=3;
+}
+ll cnt=0;
 void build(string s)
 {
-    int len=s.size();
-    int now=0;
-    int i;
+    ll len=s.size();
+    ll i;
+    ll now=0;
     wfor(i,0,len)
     {
-        int id=s[i]-'a';
+        ll id=ma[s[i]];
         if(trie[now].next[id]==-1)
         {
             trie[++cnt].init();
-            trie[cnt].s=s[i];
             trie[now].next[id]=cnt;
         }
         now=trie[now].next[id];
     }
-    trie[now].is_last++;
+    trie[now].is_string=1;
 }
-void cla_fail()
+void cal_fail()
 {
-    int i;
-    queue<int>qu;
-    wfor(i,0,26)
+    ll i;
+    ll now=0;
+    queue<ll>qu;
+    wfor(i,0,4)
     {
-        if(trie[0].next[i]!=-1)
+        if(trie[now].next[i]!=-1)
         {
-            int p=trie[0].next[i];
-            trie[p].fail=0;
-            qu.push(p);
+            trie[trie[now].next[i]].fail=0;
+            qu.push(trie[now].next[i]);
         }
     }
     while(!qu.empty())
     {
-        int father=qu.front();
+        ll father=qu.front();
         qu.pop();
-        wfor(i,0,26)
+        wfor(i,0,4)
         {
-            if(trie[father].next[i]!=-1)
+            ll child=trie[father].next[i];
+            if(child!=-1)
             {
-                int child=trie[father].next[i];
-                int p=trie[father].fail;
-                while(p!=-1&&trie[p].next[i]==-1)
-                    p=trie[p].fail;
-                if(p==-1)
+                now=trie[father].fail;
+                while(now!=-1&&trie[now].next[i]==-1)
+                {
+                    now=trie[now].fail;
+                }
+                if(now==-1)
                     trie[child].fail=0;
                 else
-                    trie[child].fail=trie[p].next[i];
+                {
+                    trie[child].fail=trie[now].next[i];
+                    if(trie[trie[now].next[i]].is_string!=0)
+                        trie[child].is_string=1;
+                }
                 qu.push(child);
             }
         }
     }
 }
-int query(string s)
+struct Matix
 {
-    int len=s.size();
-    int i;
-    int now=0;
-    int ans=0;
-    wfor(i,0,len)
+    ll m[105][105];
+};
+Matix mul(Matix a,Matix b)
+{
+    Matix c;
+    memset(c.m,0,sizeof(c.m));
+    ll i,j,k;
+    wfor(i,0,cnt)
     {
-        int id=s[i]-'a';
-        while(now!=0&&trie[now].next[id]==-1)
-            now=trie[now].fail;
-        int temp;
-        now=trie[now].next[id];
-        if(now!=-1)
-            temp=now;
-        else
-            temp=now=0;
-        while(temp!=0&&trie[temp].is_last!=-1)
+        wfor(j,0,cnt)
         {
-            ans+=trie[temp].is_last;
-            trie[temp].is_last=-1;
-            temp=trie[temp].fail;
+            wfor(k,0,cnt)
+            {
+                c.m[i][j]+=a.m[i][k]*b.m[k][j]%mod;
+                c.m[i][j]%=mod;
+            }
         }
+    }
+    return c;
+}
+Matix ksm(Matix a,ll b)
+{
+    Matix ans;
+    memset(ans.m,0,sizeof(ans.m));
+    ll i;
+    wfor(i,0,cnt)
+    {
+        ans.m[i][i]=1;
+    }
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=mul(ans,a);
+        }
+        b>>=1;
+        a=mul(a,a);
     }
     return ans;
 }
@@ -116,24 +145,65 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int t;
-    cin>>t;
-    while(t--)
+    ll n,m;
+    cin>>n>>m;
+    init();
+    ll i;
+    trie[0].init();
+    wfor(i,0,n)
     {
-        trie[0].init();
-        int n;
-        cin>>n;
-        int i;
         string s;
-        wfor(i,0,n)
-        {
-            cin>>s;
-            build(s);
-        }
-        cla_fail();
         cin>>s;
-        int ans=query(s);
-        cout<<ans<<endl;
+        build(s);
     }
+    cal_fail();
+    cnt++;
+    Matix x;
+    memset(x.m,0,sizeof(x.m));
+    wfor(i,0,cnt)
+    {
+        ll j;
+        if(trie[i].is_string==0)
+        {
+            wfor(j,0,4)
+            {
+                ll now=i;
+                if(trie[now].next[j]==-1)
+                {
+                    while(now!=-1&&trie[now].next[j]==-1)
+                        now=trie[now].fail;
+                    if(now!=-1)
+                    {
+                        if(trie[now].next[j]!=-1&&trie[trie[now].next[j]].is_string==0)
+                        {
+                            x.m[i][trie[now].next[j]]++;
+                        }
+                    }else
+                        x.m[i][0]++;
+                }else
+                {
+                    if(trie[trie[now].next[j]].is_string==0)
+                        x.m[i][trie[now].next[j]]++;
+                }
+            }
+        }
+    }
+    wfor(i,0,cnt)
+    {
+        ll j;
+        wfor(j,0,cnt)
+        {
+            cout<<x.m[i][j];
+        }
+        cout<<endl;
+    }
+    x=ksm(x,m);
+    ll ans=0;
+    wfor(i,0,cnt)
+    {
+        ans+=x.m[0][i];
+        ans%=mod;
+    }
+    cout<<ans<<endl;
     return 0;
 }
