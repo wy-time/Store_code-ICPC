c03943ff6bfd0ca5dfdf7ddbe141601a43d27717
wy-time <1142942494@qq.com>
2019-05-14 20:32:37 +0800

    nowcoder

--- debug/main.cpp
+++ debug/main.cpp
@@ -4,12 +4,79 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(int &x) {
+// void read(ll &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-char ma[55][55];
+const ll maxn=1e5+5;
+ll tree[maxn<<2];
+ll target[maxn<<2];
+void push_up(ll id)
+{
+    tree[id]=tree[id<<1]+tree[id<<1|1];
+}
+void build(ll l,ll r,ll id)
+{
+    if(l==r)
+    {
+        tree[id]=l;
+        return ;
+    }
+    ll mid=(l+r)>>1;
+    build(l,mid,id<<1);
+    build(mid+1,r,id<<1|1);
+    push_up(id);
+}
+void push_down(ll l,ll r,ll id)
+{
+    if(target[id]!=0)
+    {
+        ll mid=(l+r)>>1;
+        ll fir=target[id];
+        ll n=mid-l+1;
+        tree[id<<1]=fir*n+(n*n-n)/2;
+        target[id<<1]=fir;
+        n=r-mid;
+        fir=mid+fir;
+        tree[id<<1|1]=fir*n+(n*n-n)/2;
+        target[id<<1|1]=fir;
+        target[id]=0;
+    }
+}
+void updata(ll l,ll r,ll L,ll R,ll id)
+{
+    if(l>=L&&r<=R)
+    {
+        ll n=r-l+1;
+        ll fir=l-L+1;
+        tree[id]=fir*n+(n*n-n)/2;
+        target[id]=fir;
+        return;
+    }
+    ll mid=(l+r)>>1;
+    push_down(l,r,id);
+    if(mid>=L)
+        updata(l,mid,L,R,id<<1);
+    if(mid<R)
+        updata(mid+1,r,L,R,id<<1|1);
+    push_up(id);
+}
+ll query(ll l,ll r,ll L,ll R,ll id)
+{
+    if(l>=L&&r<=R)
+    {
+        return tree[id];
+    }
+    ll mid=(l+r)>>1;
+    push_down(l,r,id);
+    ll ans=0;
+    if(mid>=L)
+        ans+=query(l,mid,L,R,id<<1);
+    if(mid<R)
+        ans+=query(mid+1,r,L,R,id<<1|1);
+    return ans;
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -20,55 +87,22 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n;
-    cin>>n;
-    int i;
-    wfor(i,0,n)
+    ll n,m;
+    cin>>n>>m;
+    ll i;
+    build(1,n,1);
+    wfor(i,0,m)
     {
-        int j;
-        wfor(j,0,n)
+        ll op,l,r;
+        cin>>op>>l>>r;
+        if(op==1)
         {
-            cin>>ma[i][j];
+            updata(1,n,l,r,1);
+        }else
+        {
+            ll ans=query(1,n,l,r,1);
+            cout<<ans<<endl;
         }
     }
-    int flag=1;
-    wfor(i,0,n)
-    {
-        int j;
-        if(flag==0)
-        {
-            break;
-        }
-        wfor(j,0,n)
-        {
-            if(ma[i][j]=='.')
-            {
-                if(i+2<n&&j-1>=0&&j+1<n)
-                {
-                    if(ma[i+1][j]=='.'&&ma[i+2][j]=='.'&&ma[i+1][j+1]=='.'&&ma[i+1][j-1]=='.')
-                    {
-                        ma[i+1][j]='#';
-                        ma[i+2][j]='#';
-                        ma[i+1][j+1]='#';
-                        ma[i+1][j-1]='#';
-                        ma[i][j]='#';
-                    }else
-                    {
-                        flag=0;
-                        break;
-                    }
-                }else
-                {
-                    flag=0;
-                    break;
-                }
-            }
-        }
-    }
-    if(flag==0)
-    {
-        cout<<"NO"<<endl;
-    }else
-        cout<<"YES"<<endl;
     return 0;
 }
