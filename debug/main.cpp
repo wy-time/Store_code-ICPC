ec4d908759fbce19279b8b37b65b462170cce3b6
wy-time <1142942494@qq.com>
2019-05-15 21:51:20 +0800

    cf-1153-A

--- debug/main.cpp
+++ debug/main.cpp
@@ -4,14 +4,11 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(ll &x) {
+// void read(int &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const ll maxn=3e5+5;
-ll dp[maxn][3];
-ll num[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -22,24 +19,24 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    ll n,x;
-    cin>>n>>x;
-    ll i;
-    wfor(i,1,n+1)
+    int n,m;
+    cin>>n>>m;
+    int i;
+    int che[2]={0};
+    int key[2]={0};
+    wfor(i,0,n)
     {
-        cin>>num[i];
+        int t;
+        cin>>t;
+        che[t%2]++;
     }
-    dp[0][0]=0;
-    dp[0][1]=0;
-    dp[0][2]=0;
-    ll ans=0;
-    wfor(i,1,n+1)
+    wfor(i,0,m)
     {
-        dp[i][0]=max(0ll,dp[i-1][0]+num[i]);
-        dp[i][1]=max(0ll,max(dp[i-1][0],dp[i-1][1])+num[i]*x);
-        dp[i][2]=max(0ll,max(dp[i-1][0],max(dp[i-1][1],dp[i-1][2]))+num[i]);
-        ans=max(dp[i][2],max(dp[i][1],max(dp[i][0],ans)));
+        int t;
+        cin>>t;
+        key[t%2]++;
     }
+    int ans=min(che[0],key[1])+min(che[1],key[0]);
     cout<<ans<<endl;
     return 0;
 }
