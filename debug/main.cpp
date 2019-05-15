6ee6035d0a939eeb06e480a11a1529073a2e7932
wy-time <1142942494@qq.com>
2019-05-15 19:30:14 +0800

    cf-1155-c

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include <string> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(int &x) {
+// void read(ll &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+const ll maxn=3e5+5;
+ll p[maxn];
+ll gcd(ll a,ll b)
+{
+    return b==0?a:gcd(b,a%b);
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -20,39 +25,39 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n;
-    cin>>n;
-    string s;
-    cin>>s;
-    int i;
-    int cnt=(n-11)/2;
-    int len=n;
-    wfor(i,0,len)
+    ll n,m;
+    cin>>n>>m;
+    ll i;
+    ll g=0;
+    ll t;
+    cin>>t;
+    ll last=t;
+    ll beg=t;
+    wfor(i,1,n)
     {
-        if(cnt<=0)
-            break;
-        if(s[i]=='8')
+        cin>>t;
+        g=gcd(t-last,g);
+        last=t;
+    }
+    ll ans=0;
+    ll flag=0;
+    wfor(i,0,m)
+    {
+        cin>>p[i];
+    }
+    wfor(i,0,m)
+    {
+        if(g%p[i]==0)
         {
-            s.erase(i,1);
-            i--;
-            len--;
-            cnt--;
+            flag=1;
+            ans=i+1;
         }
     }
-    n=(n-11)/2;
-    wfor(i,0,n)
+    if(flag==1)
     {
-        if(s[i]!='8')
-        {
-            s.erase(i,1);
-            i--;
-            n--;
-        }else
-            break;
-    }
-    if(s[0]=='8')
         cout<<"YES"<<endl;
-    else
+        cout<<beg<<" "<<ans<<endl;
+    }else
         cout<<"NO"<<endl;
     return 0;
 }
