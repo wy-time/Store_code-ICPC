1edcc9996f48690886c7b4e9b648f59e55f845c6
wy-time <1142942494@qq.com>
2019-05-16 19:21:15 +0800

    cf-1152-C

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <cmath> 
-#include <bitset> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(int &x) {
+// void read(ll &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+ll gcd(ll a,ll b)
+{
+    return b==0?a:gcd(b,a%b);
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -21,39 +23,21 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    unsigned long n;
-    cin>>n;
-    bitset<20> bs(n);
-    int len=log2(n);
-    int i;
-    int ans[50]={0};
-    int cnt=0;
-    int num=0;
-    mfor(i,len,0)
+    ll a,b;
+    cin>>a>>b;
+    ll now=a*b/gcd(a,b);
+    ll k=0;
+    ll ans=0;
+    while(a+k<=now&&b+k<=now&&k<10000000)
     {
-        if(bs.count()==len+1)
-            break;
-        if(bs[i]==0)
+        ll t=(a+k)*(b+k)/gcd(a+k,b+k);
+        if(t<now)
         {
-            ans[cnt++]=i+1;
-            bs^=((1<<(i+1))-1);
-            num++;
-            if(bs.count()==len+1)
-                break;
-            num++;
-            n=bs.to_ulong();
-            n++;
-            bitset<20>temp(n);
-            bs=temp;
+            now=t;
+            ans=k;
         }
+        k++;
     }
-    if(cnt==0)
-        cout<<0<<endl;
-    else
-    {
-        cout<<num<<endl;
-        wfor(i,0,cnt)
-            cout<<ans[i]<<" ";
-    }
+    cout<<ans<<endl;
     return 0;
 }
