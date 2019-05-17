c8bb24bd67dce7f9ba1c8744612ffc3066176dcd
wy-time <1142942494@qq.com>
2019-05-17 21:15:49 +0800

    cf-1167-C

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -10,6 +9,31 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+const int maxn=1e6+5;
+int pre[maxn];
+int findx(int x)
+{
+    int t=x;
+    while(pre[t]!=t)
+    {
+        t=pre[t];
+    }
+    int temp;
+    while(pre[x]!=t)
+    {
+        temp=pre[x];
+        pre[x]=t;
+        x=temp;
+    }
+    return t;
+}
+void add(int a,int b)
+{
+    int a1=findx(a);
+    int b1=findx(b);
+    pre[b1]=a1;
+}
+int ans[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -17,36 +41,42 @@ int main()
     freopen("F:\\Desktop\\question\\in.txt","r",stdin);
     #endif
     #ifdef ubuntu
-freopen("/home/time/debug/debug/in","r",stdin);
-freopen("/home/time/debug/debug/out","w",stdout);
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int num[6]={4,8,15,16,23,42};
-    int t[4]={0};
+    int n,m;
+    cin>>n>>m;
     int i;
-    wfor(i,1,5)
+    wfor(i,0,maxn)
     {
-        cout<<"? "<<1<<" "<<i+1<<endl;
-        cin>>t[i-1];
+        pre[i]=i;
     }
-    do
+    wfor(i,1,m+1)
     {
-        int flag=1;
-        wfor(i,0,4)
+        int cnt;
+        cin>>cnt;
+        int j;
+        wfor(j,0,cnt)
         {
-            if(num[0]*num[i+1]!=t[i])
+            int temp;
+            cin>>temp;
+            if(findx(i)!=findx(temp+m+1))
             {
-                flag=0;
-                break;
+                if(pre[temp+m+1]==temp+m+1)
+                    ans[i]++;
+                else
+                    ans[i]+=ans[findx(temp+m+1)];
+                add(i,temp+m+1);
             }
         }
-        if(flag==1)
-        {
-            cout<<"! ";
-            wfor(i,0,6)
-                cout<<num[i]<<" ";
-            cout<<endl;
-            break;
-        }
-    }while(next_permutation(num,num+6));
+    }
+    wfor(i,1,n+1)
+    {
+        int root=findx(i+m+1);
+        if(ans[root]!=0)
+            cout<<ans[root]<<" ";
+        else
+            cout<<1<<" ";
+    }
     return 0;
 }
