6afb018a282b869ae9af40dbcda0fa47b8cb0c65
wy-time <1142942494@qq.com>
2019-05-21 19:26:25 +0800

    hdu-1048

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstring> 
-#include <algorithm> 
+#include <map> 
+#include <string> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -11,9 +11,6 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=5e5+5;
-pair<int,int>num[maxn];
-int len[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -24,40 +21,49 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n;
-    int casecnt=0;
-    while(cin>>n)
+    string s;
+    map<char,char>ma;
+    ma.insert(make_pair('A','V'));
+    ma.insert(make_pair('B','W'));
+    ma.insert(make_pair('C','X'));
+    ma.insert(make_pair('D','Y'));
+    ma.insert(make_pair('E','Z'));
+    ma.insert(make_pair('F','A'));
+    ma.insert(make_pair('G','B'));
+    ma.insert(make_pair('H','C'));
+    ma.insert(make_pair('I','D'));
+    ma.insert(make_pair('J','E'));
+    ma.insert(make_pair('K','F'));
+    ma.insert(make_pair('L','G'));
+    ma.insert(make_pair('M','H'));
+    ma.insert(make_pair('N','I'));
+    ma.insert(make_pair('O','J'));
+    ma.insert(make_pair('P','K'));
+    ma.insert(make_pair('Q','L'));
+    ma.insert(make_pair('R','M'));
+    ma.insert(make_pair('S','N'));
+    ma.insert(make_pair('T','O'));
+    ma.insert(make_pair('U','P'));
+    ma.insert(make_pair('V','Q'));
+    ma.insert(make_pair('W','R'));
+    ma.insert(make_pair('X','S'));
+    ma.insert(make_pair('Y','T'));
+    ma.insert(make_pair('Z','U'));
+    while(cin>>s)
     {
-        casecnt++;
+        if(s=="ENDOFINPUT")
+            break;
+        getline(cin,s);
+        getline(cin,s);
+        int len=s.size();
         int i;
-        wfor(i,0,n)
+        wfor(i,0,len)
         {
-            cin>>num[i].first>>num[i].second;
+            if(ma.count(s[i])!=0)
+                s[i]=ma[s[i]];
         }
-        sort(num,num+n);
-        int ans=1;
-        len[1]=num[0].second;
-        wfor(i,1,n)
-        {
-            if(num[i].second>len[ans])
-            {
-                ans++;
-                len[ans]=num[i].second;
-            }else
-            {
-                int pos=lower_bound(len,len+ans,num[i].second)-len;
-                len[pos]=num[i].second;
-            }
-        }
-        cout<<"Case "<<casecnt<<":"<<endl<<"My king, at most ";
-        if(ans>1)
-        {
-            cout<<ans<<" roads can be built."<<endl;
-        }else
-        {
-            cout<<ans<<" road can be built."<<endl;
-        }
-        cout<<endl;
+        cout<<s<<endl;
+        cin>>s;
     }
     return 0;
 }
