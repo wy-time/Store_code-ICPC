fe9319b5b6b93ae7510f9847e48d85dc663fc113
wy-time <1142942494@qq.com>
2019-06-16 16:09:10 +0800

    cf-1156-c

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring> 
 #include <algorithm> 
 #include <cstdio>
 using namespace std;
@@ -13,22 +12,6 @@ typedef long long ll;
 // }
 const int maxn=2e5+5;
 int num[maxn];
-int num2[maxn];
-int vis[maxn];
-void add(int x)
-{
-    int i;
-    for(i=x;i<maxn;i+=(i&-i))
-        vis[i]++;
-}
-int get_sum(int x)
-{
-    int i;
-    int sum=0;
-    for(i=x;i>0;i-=(i&-i))
-        sum+=vis[i];
-    return sum;
-}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -45,52 +28,27 @@ int main()
     wfor(i,1,n+1)
     {
         cin>>num[i];
-        num2[i]=num[i];
     }
-    sort(num+1,num+1+n);
-    sort(num2+1,num2+1+n);
-    int ans=0;
-    int use=0;
-    wfor(i,1,n)
+    sort(num+1,num+n+1);
+    int l=1;
+    int r=n/2+1;
+    while(l<=r)
     {
-        if(get_sum(i)-get_sum(i-1)==1)
-            continue;
-        use++;
-        add(i);
-        int pos=lower_bound(num+1,num+n+1,num[i]+k)-num;
-        int last=n-pos+1-(use-get_sum(pos-1));
-        if(last>0)
+        int mid=(l+r)>>1;
+        int flag=1;
+        wfor(i,1,mid+1)
         {
-            ans++;
-            use++;
-            add(n-last+1);
-        }else
-            break;
+            if(num[n-mid+i]-num[i]<k)
+            {
+                flag=0;
+                break;
+            }
+        }
+        if(flag)
+            l=mid+1;
+        else
+            r=mid-1;
     }
-    memset(vis,0,sizeof(vis));
-    sort(num+1,num+1+n,greater<int>());
-    use=0;
-    int temp=0;
-    wfor(i,1,n)
-    {
-        if(get_sum(i)-get_sum(i-1)==1)
-            continue;
-        use++;
-        add(i);
-        int pos=upper_bound(num2+1,num2+n+1,num[i]-k)-num2-1;
-        if(pos==0)
-            break;
-        pos=n-pos+1;
-        int last=n-pos+1-(use-get_sum(pos-1));
-        if(last>0)
-        {
-            temp++;
-            use++;
-            add(n-last+1);
-        }else
-            break;
-    }
-    ans=max(temp,ans);
-    cout<<ans<<endl;
+    cout<<r<<endl;
     return 0;
 }
