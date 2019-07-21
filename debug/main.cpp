a676671f46cb8edb93040d3a2972c2a52d49fe94
wy-time <1142942494@qq.com>
2019-07-21 14:52:42 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -4,17 +4,23 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(ll &x) {
+// void read(int &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const ll maxn=1005;
-ll num[maxn];
-ll ans[maxn];
-ll gcd(ll a,ll b)
+const ll mod=1e9+7;
+ll ksm(ll a,ll b)
 {
-    return b==0?a:gcd(b,a%b);
+    ll ans=1;
+    while(b)
+    {
+        if(b&1)
+            ans=ans*a%mod;
+        a=a*a%mod;
+        b>>=1;
+    }
+    return ans;
 }
 int main()
 {
@@ -26,44 +32,14 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    ll n,m;
-    while(cin>>n>>m)
+    ll n;
+    while(cin>>n)
     {
-        ll i;
-        wfor(i,0,n)
-        {
-            cin>>num[i];
-        }
         if(n==1)
-        {
-            cout<<"Yes"<<endl;
-            cout<<m<<endl;
-        }else
-        {
-            ll value=num[0]*num[1]/gcd(num[0],num[1]);
-            wfor(i,2,n)
-            {
-                value=value*num[i]/gcd(value,num[i]);
-            }
-            ll sum=0;
-            wfor(i,0,n)
-            {
-                ans[i]=value/num[i];
-                sum+=ans[i];
-            }
-            ll mul=1;
-            if(m%sum==0)
-            {
-                mul=m/sum;
-                cout<<"Yes"<<endl;
-                wfor(i,0,n-1)
-                {
-                    cout<<ans[i]*mul<<" ";
-                }
-                cout<<ans[i]*mul<<endl;
-            }else
-            cout<<"No"<<endl;
-        }
+            cout<<1<<endl;
+        else
+            cout<<((n+1)%mod*ksm(n*n,mod-2)%mod)<<endl;
+
     }
     return 0;
 }
