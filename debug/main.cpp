ec3a0279d16dcb5db59f605bbda71d48b6ecb732
wy-time <1142942494@qq.com>
2019-07-21 16:13:38 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -9,19 +9,9 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const ll mod=1e9+7;
-ll ksm(ll a,ll b)
-{
-    ll ans=1;
-    while(b)
-    {
-        if(b&1)
-            ans=ans*a%mod;
-        a=a*a%mod;
-        b>>=1;
-    }
-    return ans;
-}
+const int maxn=1e5+5;
+int num[maxn];
+int dp[105][105];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -32,14 +22,33 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    ll n;
-    while(cin>>n)
+    int n,c;
+    cin>>n>>c;
+    int i;
+    wfor(i,0,n)
     {
-        if(n==1)
-            cout<<1<<endl;
-        else
-            cout<<((n+1)%mod*ksm(n*n,mod-2)%mod)<<endl;
-
+        cin>>num[i];
     }
+    int ans=1;
+    wfor(i,0,c+1)
+    {
+        if(num[0]==i)
+            dp[num[0]][i]=0;
+        else
+            dp[num[0]][i]=1;
+    }
+    wfor(i,1,n)
+    {
+        int j;
+        wfor(j,0,c+1)
+        {
+            if(num[i]==j)
+                dp[num[i]][j]=0;
+            else
+                dp[num[i]][j]=dp[j][num[i]]+1;
+            ans=max(ans,dp[num[i]][j]);
+        }
+    }
+    cout<<ans<<endl;
     return 0;
 }
