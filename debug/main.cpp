c9b246ca0d612298ba49912194e61d402bf5f66e
wy-time <1142942494@qq.com>
2019-08-03 19:24:20 +0800

    nowcoder

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <vector> 
-#include <queue> 
-#include <cstring> 
-#include <string> 
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <string>
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(ll &x) {
-// 	char ch = getchar(); x = 0;
-// 	for (; ch < '0' || ch > '9'; ch = getchar());
-// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+//  char ch = getchar(); x = 0;
+//  for (; ch < '0' || ch > '9'; ch = getchar());
+//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const ll maxn = 100005;// n(空间复杂度o(n*ALP)),实际开n即可
+const ll maxn = 100005;// n(?????o(n*ALP)),???n??
 const ll ALP = 26;
 int vis[maxn];
-struct PAM { // 每个节点代表一个回文串
-     ll next[maxn][ALP];// next指针,参照Trie树
-     ll fail[maxn]; // fail失配后缀链接
-     ll cnt[maxn]; // 此回文串出现个数
+struct PAM { // ???????????
+     ll next[maxn][ALP];// next??,??Trie?
+     ll fail[maxn]; // fail??????
+     ll cnt[maxn]; // ????????
      ll num[maxn];
-     ll len[maxn]; // 回文串长度
-     ll s[maxn]; // 存放添加的字符
-     ll last; //指向上一个字符所在的节点，方便下一次add
-     ll n; // 已添加字符个数
-     ll p; // 节点个数
-     ll newnode(ll w) { // 初始化节点，w=长度
+     ll len[maxn]; // ?????
+     ll s[maxn]; // ???????
+     ll last; //??????????????????add
+     ll n; // ???????
+     ll p; // ????
+     ll newnode(ll w) { // ??????w=??
      for (ll i = 0; i < ALP; i++)
         next[p][i] = 0;
         cnt[p] = 0;
@@ -40,10 +40,10 @@ struct PAM { // 每个节点代表一个回文串
          newnode(-1);
          last = 0;
          n = 0;
-         s[n] = -1; // 开头放一个字符集中没有的字符，减少特判
+         s[n] = -1; // ???????????????????
          fail[0] = 1;
      }
-     ll get_fail(ll x) { // 和KMP一样，失配后找一个尽量最长的
+     ll get_fail(ll x) { // ?KMP??????????????
          while (s[n - len[x] - 1] != s[n]) x = fail[x];
          return x;
      }
@@ -61,9 +61,9 @@ struct PAM { // 每个节点代表一个回文串
          cnt[last]++;
      }
      void count() {
-         // 最后统计一遍每个节点出现个数
-         // 父亲累加儿子的cnt,类似SAM中parent树
-         // 满足parent拓扑关系
+         // ??????????????
+         // ???????cnt,??SAM?parent?
+         // ??parent????
          for (ll i = p - 1; i >= 0; i--)
              cnt[fail[i]] += cnt[i];
      }
@@ -73,23 +73,27 @@ ll ans=0;
 int vis2[maxn];
 void slove(ll su,ll now)
 {
+    vis[now]=1;
     vector<int>pre;
     ll t=pam.fail[now];
-    while(t>1&&!vis[t])
+    while(t>1&&!vis2[t])
     {
-        sun[now]++;
-        vis[t]=1;
-        pre.push_back(t);
+        if(t!=su)
+        {
+            sun[now]++;
+            vis2[t]=1;
+            pre.push_back(t);
+        }
         t=pam.fail[t];
     }
     sun[now]+=sun[su];
-    ans+=sun[now];
-    if(now>1)
+    if(su>1)
     {
         sun[now]++;
-        vis[now]=1;
+        vis2[now]=1;
         pre.push_back(now);
     }
+    ans+=sun[now];
     ll j;
     wfor(j,0,ALP)
     {
@@ -97,7 +101,7 @@ void slove(ll su,ll now)
             slove(now,pam.next[now][j]);
     }
     for(auto k:pre)
-        vis[k]=0;
+        vis2[k]=0;
     pre.clear();
 }
 int main()
@@ -129,13 +133,11 @@ int main()
         {
             pam.add(s[i]);
         }
-        /*wfor(i,2,pam.p)
+        wfor(i,2,pam.p)
         {
             if(!vis[i])
                 slove(0,i);
-        }*/
-        slove(0,0);
-        slove(1,1);
+        }
         cout<<"Case #"<<casecnt<<": ";
         cout<<ans<<endl;
     }
