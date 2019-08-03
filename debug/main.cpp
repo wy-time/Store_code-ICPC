01af3033af8dc0f71f3692c18a7d19f5d26bf565
wy-time <1142942494@qq.com>
2019-08-03 19:57:04 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,143 +1,141 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <cstring>
-#include <string>
-#include <cstdio>
-using namespace std;
-typedef long long ll;
+#include <bits/stdc++.h>
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(ll &x) {
-//  char ch = getchar(); x = 0;
-//  for (; ch < '0' || ch > '9'; ch = getchar());
-//  for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-// }
-const ll maxn = 100005;// n(空间复杂度o(n*ALP)),实际开n即可
-const ll ALP = 26;
-int vis[maxn];
-struct PAM { // 每个节点代表一个回文串
-    ll next[maxn][ALP];// next指针,参照Trie树
-    ll fail[maxn]; // fail失配后缀链接
-    ll cnt[maxn]; // 此回文串出现个数
-    ll num[maxn];
-    ll len[maxn]; // 回文串长度
-    ll s[maxn]; // 存放添加的字符
-    ll last; //指向上一个字符所在的节点，方便下一次add
-    ll n; // 已添加字符个数
-    ll p; // 节点个数
-    ll newnode(ll w) { // 初始化节点，w=长度
-        for (ll i = 0; i < ALP; i++)
-            next[p][i] = 0;
-        cnt[p] = 0;
-        num[p] = 0;
-        len[p] = w;
-        return p++;
-    }
-    void init() {
-        p = 0;
-        newnode(0);
-        newnode(-1);
-        last = 0;
-        n = 0;
-        s[n] = -1; // 开头放一个字符集中没有的字符，减少特判
-        fail[0] = 1;
-    }
-    ll get_fail(ll x) { // 和KMP一样，失配后找一个尽量最长的
-        while (s[n - len[x] - 1] != s[n]) x = fail[x];
-        return x;
-    }
-    void add(ll c) {
-        c -= 'a';
-        s[++n] = c;
-        ll cur = get_fail(last);
-        if (!next[cur][c]) {
-            ll now = newnode(len[cur] + 2);
-            fail[now] = next[get_fail(fail[cur])][c];
-            next[cur][c] = now;
-            num[now] = num[fail[now]] + 1;
-        }
-        last = next[cur][c];
-        cnt[last]++;
-    }
-    void count() {
-        // 最后统计一遍每个节点出现个数
-        // 父亲累加儿子的cnt,类似SAM中parent树
-        // 满足parent拓扑关系
-        for (ll i = p - 1; i >= 0; i--)
-            cnt[fail[i]] += cnt[i];
-    }
-} pam;
-ll sun[maxn];
-ll ans = 0;
-int vis2[maxn];
-void slove(ll su, ll now)
-{
-    vector<int>pre;
-    ll t = pam.fail[now];
-    while (t > 1 && !vis[t])
-    {
-        sun[now]++;
-        vis[t] = 1;
-        pre.push_back(t);
-        t = pam.fail[t];
-    }
-    sun[now] += sun[su];
-    ans += sun[now];
-    if (now > 1)
-    {
-        sun[now]++;
-        vis[now] = 1;
-        pre.push_back(now);
-    }
-    ll j;
-    wfor(j, 0, ALP)
-    {
-        if (pam.next[now][j] != 0)
-            slove(now, pam.next[now][j]);
-    }
-    for (auto k : pre)
-        vis[k] = 0;
-    pre.clear();
+typedef long long LL;
+void read(LL &x) {
+char ch = getchar(); x = 0;
+for (; ch < '0' || ch > '9'; ch = getchar());
+for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 }
-int main()
+using namespace std;
+const double eps=1e-9;
+const int maxn = 100001;
+const int MAX=1e4+5;
+LL prime[MAX];
+int equa(double a,double b)
 {
-    std::ios::sync_with_stdio(false);
-#ifdef test
-    freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
-#endif
-#ifdef ubuntu
-    freopen("/home/time/debug/debug/in", "r", stdin);
-    freopen("/home/time/debug/debug/out", "w", stdout);
-#endif
-    ll t;
-    cin >> t;
-    ll casecnt = 0;
-    while (t--)
+    if(fabs(a-b)<eps)
+        return 1;
+    else
+        return 0;
+}
+void get_prime()
+{
+    LL i;
+    wfor(i,2,MAX)
     {
-        casecnt++;
-        memset(vis, 0, sizeof(vis));
-        memset(vis2, 0, sizeof(vis2));
-        memset(sun, 0, sizeof(sun));
-        pam.init();
-        string s;
-        cin >> s;
-        ll len = s.size();
-        ll i;
-        ans = 0;
-        wfor(i, 0, len)
+        if(prime[i]==0)
+            prime[++prime[0]]=i;
+        int j;
+        for(j=1;j<=prime[0]&&prime[j]*i<MAX;j++)
         {
-            pam.add(s[i]);
+            prime[prime[j]*i]=1;
+            if(i%prime[j]==0)
+                break;
         }
-        /*wfor(i,2,pam.p)
+    }
+}
+LL mul(LL a,LL b,LL mod) {
+    if(!a) return 0;
+    return ((a&1)*b%mod + (mul(a>>1,b,mod)<<1)%mod)%mod;
+}
+LL quickPow(LL a,LL d,LL n) {
+    LL ret = 1;
+    while(d) {
+        if(d&1) ret = mul(ret,a,n);
+        d >>= 1;
+        a = mul(a,a,n);
+    }
+    return ret;
+}
+bool check(LL a,LL d,LL n) {
+    if(n == a) return true;
+    while(~d&1) d >>= 1;
+    LL t = quickPow(a,d,n);
+    while(d < n-1 && t != 1 && t != n-1) {
+        t = mul(t,t,n);
+        d <<= 1;
+    }
+    return (d&1) || t == n-1;
+}
+bool isP(LL n) {
+    if(n == 2) return true;
+    if(n < 2 || 0 == (n&1)) return false;
+    static int p[5] = {2,3,7,61,24251};
+    for(int i = 0; i < 5; ++i)
+        if(!check(p[i],n-1,n)) return false;
+    return true;
+}
+int main() {
+    #ifdef test
+    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+    #endif
+    #ifdef ubuntu
+    freopen("/home/time/debug/debug/in","r",stdin);
+    freopen("/home/time/debug/debug/out","w",stdout);
+    #endif   
+    LL x;
+    LL t;
+    get_prime();
+    //scanf("%d",&t);
+    read(t);
+    while(t--){
+        read(x);
+        if(x == 1) {
+            puts("1");
+            continue;
+        }
+        if(isP(x)){
+            puts("1");
+            continue;
+        }
+        int i;
+        int res=100000000;
+        for(i=1;i<=prime[0]&&prime[i]*prime[i]<=x;i++)
         {
-            if(!vis[i])
-                slove(0,i);
-        }*/
-        slove(0, 0);
-        slove(1, 1);
-        cout << "Case #" << casecnt << ": ";
-        cout << ans << endl;
+            if(x%prime[i]==0)
+            {
+                int cnt=0;
+                while(x%prime[i]==0)
+                {
+                    cnt++;
+                    x/=prime[i];
+                }
+                res=min(res,cnt);
+                if(res==1)
+                    break;
+            }
+        }
+        if(res==1)
+        {
+            puts("1");
+            continue;
+        }
+        if(x == 1) {
+            printf("%d\n",res);
+            continue;
+        }
+        if(isP(x)){
+            puts("1");
+            continue;
+        }
+        double temp=pow(x,-4.0);
+        if(equa(temp*temp*temp*temp,x))
+        {
+            res=min(res,4);
+            temp=pow(x,-2.0);
+            if(equa(temp*temp,x))
+                res=min(2,res);
+        }else
+        {
+            temp=pow(x,-3.0);
+            if(equa(temp*temp*temp,x))
+            {
+                res=min(res,3);
+            }else
+                res=1;
+        }
+        printf("%d\n",res);
     }
     return 0;
 }
