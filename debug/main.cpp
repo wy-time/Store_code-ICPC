88324ee6d6a66056798f5c4baa4fc6c3dba51985
wy-time <1142942494@qq.com>
2019-08-07 21:01:16 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,95 +1,180 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring> 
+#include <algorithm> 
+#include <cstdio>
+using namespace std;
+typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-typedef long long LL;
-void read(LL &x) {
-char ch = getchar(); x = 0;
-for (; ch < '0' || ch > '9'; ch = getchar());
-for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+void read(ll &x) {
+    int flag=0;
+    char ch = getchar(); x = 0;
+    if(ch=='-')
+        flag=1;
+    for (; ch < '0' || ch > '9'; ch = getchar());
+    for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+    if(flag)
+        x*=-1;
 }
-using namespace std;
-const int maxn = 100001;
-const int MAX=1e4+5;
-int prime[MAX];
-void get_prime()
+const ll maxn=2005;
+ll ma[maxn][maxn];
+int nex[maxn][maxn];
+struct node
 {
-    int i;
-    wfor(i,2,MAX)
-    {
-        if(prime[i]==0)
-            prime[++prime[0]]=i;
-        int j;
-        for(j=1;j<=prime[0]&&prime[j]*i<MAX;j++)
-        {
-            prime[prime[j]*i]=1;
-            if(i%prime[j]==0)
-                break;
-        }
-    }
+    ll sum;
+    ll sum_L;
+    ll sum_R;
+    ll sum_tot;
+};
+struct POll
+{
+    ll x;
+    ll y;
+    ll w;
+};
+POll poll[maxn];
+ll x[maxn];
+ll y[maxn];
+ll num[maxn];
+node sum[maxn<<2];
+void push_up(int id)
+{
+    sum[id].sum_tot=sum[id<<1].sum_tot+sum[id<<1|1].sum_tot;
+    sum[id].sum=max(sum[id<<1].sum_R+sum[id<<1|1].sum_L,max(sum[id<<1].sum,sum[id<<1|1].sum));
+    sum[id].sum_L=max(sum[id<<1].sum_L,sum[id<<1].sum_tot+sum[id<<1|1].sum_L);
+    sum[id].sum_R=max(sum[id<<1|1].sum_R,sum[id<<1|1].sum_tot+sum[id<<1].sum_R);
 }
-int main() {
+void build(int id,int l,int r)
+{
+    if(l==r)
+    {
+        if(num[l]>=0)
+        {
+            sum[id].sum=num[l];
+        }else
+        {
+            sum[id].sum=0;
+        }
+        sum[id].sum_L=num[l];
+        sum[id].sum_R=num[l];
+        sum[id].sum_tot=num[l];
+        return ;
+    }
+    int mid=(l+r)>>1;
+    build(id<<1,l,mid);
+    build(id<<1|1,mid+1,r);
+    push_up(id);
+}
+void update(int id,int l,int r,int pos,int x)
+{
+    if(l==r)
+    {
+        if(num[l]+x>=0)
+        {
+            sum[id].sum+=x;
+        }else
+        {
+            sum[id].sum=0;
+        }
+        sum[id].sum_L+=x;
+        sum[id].sum_R+=x;
+        sum[id].sum_tot+=x;
+        return ;
+    }
+    int mid=(l+r)>>1;
+    if(pos<=mid)
+        update(id<<1,l,mid,pos,x);
+    else
+        update(id<<1|1,mid+1,r,pos,x);
+    push_up(id);
+}
+int main()
+{
+    std::ios::sync_with_stdio(false);
     #ifdef test
     freopen("F:\\Desktop\\question\\in.txt","r",stdin);
     #endif
     #ifdef ubuntu
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
-    #endif   
-    LL x;
-    LL t;
-    get_prime();
-    //scanf("%d",&t);
+    #endif
+    ll t;
     read(t);
-    while(t--){
-        read(x);
-        if(x == 1) {
-            puts("1");
-            continue;
-        }
-        int i;
-        int res=100000000;
-        for(i=1;i<=prime[0]&&prime[i]*prime[i]<=x;i++)
+    while(t--)
+    {
+        ll n;
+        read(n);
+        //cin>>n;
+        ll i;
+        wfor(i,0,n)
         {
-            if(x%prime[i]==0)
+            //cin>>poll[i].x>>poll[i].y>>poll[i].w;
+            read(poll[i].x);
+            read(poll[i].y);
+            read(poll[i].w);
+            x[i]=poll[i].x;
+            y[i]=poll[i].y;
+        }
+        sort(x,x+n);
+        sort(y,y+n);
+        ll len=unique(x,x+n)-x;
+        ll len2=unique(y,y+n)-y;
+        ll j;
+        wfor(i,1,len+1)
+        {
+            wfor(j,1,len2+1)
+                ma[i][j]=0;
+        }
+        wfor(i,0,n)
+        {
+            ll tx=poll[i].x;
+            ll ty=poll[i].y;
+            ll pos=lower_bound(x,x+len,tx)-x;
+            tx=pos+1;
+            pos=lower_bound(y,y+len2,ty)-y;
+            ty=pos+1;
+            ma[tx][ty]+=poll[i].w;
+        }
+        wfor(i,1,len+1)
+        {
+            int lasty=1;
+            wfor(j,2,len2+1)
             {
-                int cnt=0;
-                while(x%prime[i]==0)
+                if(ma[i][j]!=0)
                 {
-                    cnt++;
-                    x/=prime[i];
+                    nex[i][lasty]=j;
+                    lasty=j;
                 }
-                res=min(res,cnt);
-                if(res==1)
-                    break;
+            }
+            nex[i][lasty]=j;
+        }
+        ll ans=0;
+        wfor(i,1,len+1)
+        {
+            memset(num,0,sizeof(num));
+            wfor(j,i,len+1)
+            {
+                int k;
+                if(j==i)
+                {
+                    for(k=1;k<=len2;k=nex[j][k])
+                    {
+                        num[k]+=ma[j][k];
+                    }
+                    build(1,1,len2);
+                }else
+                {
+                    for(k=1;k<=len2;k=nex[j][k])
+                    {
+                        if(ma[j][k]!=0)
+                            update(1,1,len2,k,ma[j][k]);
+                    }
+                }
+                ans=max(ans,sum[1].sum);
             }
         }
-        if(res==1)
-        {
-            puts("1");
-            continue;
-        }
-        if(x == 1) {
-            printf("%d\n",res);
-            continue;
-        }
-        LL temp=sqrt(x);
-        if(temp*temp==x)
-        {
-            LL temp2=sqrt(temp);
-            if(temp2*temp2==temp)
-                res=min(res,4);
-            else
-                res=min(2,res);
-        }else
-        {
-            temp=pow(x,1.0/3.0);
-            if(temp*temp*temp==x||(temp+1)*(temp+1)*(temp+1)==x||(temp-1)*(temp-1)*(temp-1)==x)
-            {
-                res=min(res,3);
-            }else
-                res=1;
-        }
-        printf("%d\n",res);
+        printf("%lld\n",ans);
+        //cout<<ans<<endl;
     }
     return 0;
 }
--- debug/test.cpp
+++ debug/test.cpp
@@ -1,155 +1,42 @@
 #include <iostream>
-
-#include <bitset> 
-
 #include <cstdio>
-
+#include <ctime>
+#include <random>
+#include <set>
 using namespace std;
-
 typedef long long ll;
-
 #define wfor(i,j,k) for(i=j;i<k;++i)
-
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-
-// void read(ll &x) {
-
+#define random(a,b) (1ll*(a)+rnd()%((b)-(a)+1))
+// void read(int &x) {
 // 	char ch = getchar(); x = 0;
-
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
-
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-
 // }
-
-ll val[28][28];
-
-ll ans[28];
-
-ll ch[14];
-
-int main()
-
+int main (int argc, char *argv[])
 {
-
-    std::ios::sync_with_stdio(false);
-
-    #ifdef test
-
-    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
-
-    #endif
-
-    #ifdef ubuntu
-
-    freopen("/home/time/debug/debug/in","r",stdin);
-
-    freopen("/home/time/debug/debug/out","w",stdout);
-
-    #endif
-
-    ll n;
-
-    cin>>n;
-
-    ll i;
-
-    wfor(i,0,2*n)
-
-    {
-
-        ll j;
-
-        wfor(j,0,2*n)
-
-        {
-
-            cin>>val[i][j];
-
-            ans[i]+=val[i][j];
-
-        }
-
-    }
-
-    string temp="";
-
-    wfor(i,0,n)
-
-        temp+='1';
-
-    wfor(i,0,n)
-
-        temp+='0';
-
-    bitset<28> ends(temp);
-
-    ll end=ends.to_ullong();
-
-    end++;
-
-    ll res=0;
-    bitset<28>gao;
-    wfor(i,0,end)
-
-    {
-
-        bitset<28> now(i);
-
-        if(now.count()==n)
-
-        {
-
-            ll cnt=0;
-
-            ll j;
-
-            ll temp=0;
-
-            wfor(j,0,2*n)
-
-            {
-
-                if(now[j]==1)
-
-                {
-
-                    ch[cnt++]=j;
-
-                    temp+=ans[j];
-
-                }
-
-            }
-
-            wfor(j,0,cnt)
-
-            {
-
-                ll k;
-
-                wfor(k,j+1,cnt)
-
-                {
-
-                    temp-=val[ch[j]][ch[k]]*2;
-
-                }
-
-            }
-
-            if(res<temp)
-            {
-                res=temp;
-                gao=now;
-            }
-
-        }
-
-    }
-
-    cout<<res<<endl;
-    cout<<gao.to_string()<<endl;
-    return 0;
-
+	#ifdef test
+	freopen("F:\\Desktop\\question\\in.txt","r",stdin);
+	#endif
+	#ifdef ubuntu
+	freopen("/home/time/debug/debug/in","w",stdout);
+	//freopen("/home/time/debug/debug/out","w",stdout);
+	#endif
+	int seed = time(NULL);
+	if (argc > 1)
+	{
+		seed = atoi(argv[1]);
+	}
+	std::mt19937 rnd(seed);
+	int i=0;
+	int j;
+	wfor(j,0,1000)
+	{
+		cout<<i<<" "<<j<<" "<<1<<endl;
+	}
+	wfor(i,1,1001)
+	{
+		cout<<i<<" "<<i<<" "<<1<<endl;
+	}
+	return 0;
 }
