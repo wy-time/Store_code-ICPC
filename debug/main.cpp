28eb410c218cb3d6f67c7b91680c786e5e9ed24d
wy-time <1142942494@qq.com>
2019-08-12 14:16:03 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -4,30 +4,23 @@ using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-void read(ll &x) {
-char ch = getchar(); x = 0;
-for (; ch < '0' || ch > '9'; ch = getchar());
-for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-}
-const ll mod=1e9+7;
-ll ksm(ll a,ll b)
+// void read(ll &x) {
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// }
+ll check(ll now,ll n,ll k,ll m)
 {
-    ll ans=1;
-    while(b)
-    {
-        if(b&1)
-            ans=ans*a%mod;
-        a=a*a%mod;
-        b>>=1;
-    }
-    return ans;
+    ll no=(n-k+1);
+    ll sorce=m/no;
+    if(now>sorce)
+        return 1;
+    else
+        return 0;
 }
-const ll maxn=500005;
-ll sum[maxn];
-ll ans[maxn];
 int main()
 {
-    //std::ios::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
     #ifdef test
     freopen("F:\\Desktop\\question\\in.txt","r",stdin);
     #endif
@@ -36,38 +29,22 @@ int main()
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
     ll t;
-    //cin>>t;
-    read(t);
+    cin>>t;
     while(t--)
     {
-        ll n,m;
-        //cin>>n>>m;
-        read(n);
-        read(m);
-        ll i;
-        sum[1]=0;
-        ans[1]=0;
-        wfor(i,0,n)
+        ll n,m,k;
+        cin>>n>>m>>k;
+        ll l=0,r=m+1;
+        while(l<=r)
         {
-            ll s,r,x,a;
-            read(r);
-            read(s);
-            read(x);
-            read(a);
-            a%=mod;
-            //cin>>r>>s>>x>>a;
-            ll temp=(s%mod*ksm(r,mod-2))%mod;
-            sum[i+2]=(((temp-1+mod)%mod)*((ans[i+1]-ans[x]+mod)%mod)%mod+temp*a%mod)%mod;
-            ans[i+2]=(sum[i+2]+ans[i+1])%mod;
+            ll mid=(l+r)>>1;
+            if(check(mid,n,k,m))
+            {
+                r=mid-1;
+            }else
+                l=mid+1;
         }
-        wfor(i,0,m)
-        {
-            ll l,r;
-            //cin>>l>>r;
-            read(l);read(r);
-            printf("%lld\n",(ans[r]-ans[l]+mod+mod)%mod);
-            //cout<<(ans[r]-ans[l]+mod)%mod<<endl;
-        }
+        cout<<l*k<<endl;
     }
     return 0;
 }
