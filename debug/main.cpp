f4fdda539ac85b396abadddf05227cca4918d7fe
wy-time <1142942494@qq.com>
2019-08-12 14:35:43 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -13,7 +13,9 @@ ll check(ll now,ll n,ll k,ll m)
 {
     ll no=(n-k+1);
     ll sorce=m/no;
-    if(now>sorce)
+    ll temp=m-sorce*no;
+    temp=n-temp;
+    if(temp*(sorce+1)<=now)
         return 1;
     else
         return 0;
@@ -34,7 +36,12 @@ int main()
     {
         ll n,m,k;
         cin>>n>>m>>k;
-        ll l=0,r=m+1;
+        if(m==0)
+        {
+            cout<<k<<endl;
+            continue;
+        }
+        ll l=0,r=(m+1)*n;
         while(l<=r)
         {
             ll mid=(l+r)>>1;
@@ -44,7 +51,7 @@ int main()
             }else
                 l=mid+1;
         }
-        cout<<l*k<<endl;
+        cout<<l<<endl;
     }
     return 0;
 }
