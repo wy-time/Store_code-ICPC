cf22d0cb23efa160db0780841fab572d33a5041d
wy-time <1142942494@qq.com>
2019-08-12 13:36:10 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include <string> 
-#include <stack> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(int &x) {
-// 	char ch = getchar(); x = 0;
-// 	for (; ch < '0' || ch > '9'; ch = getchar());
-// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-// }
-const int maxn=3005;
-int ma[maxn][maxn];
-int h[maxn][maxn];
-int sum[maxn][maxn];
+void read(ll &x) {
+char ch = getchar(); x = 0;
+for (; ch < '0' || ch > '9'; ch = getchar());
+for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+}
+const ll mod=1e9+7;
+ll ksm(ll a,ll b)
+{
+    ll ans=1;
+    while(b)
+    {
+        if(b&1)
+            ans=ans*a%mod;
+        a=a*a%mod;
+        b>>=1;
+    }
+    return ans;
+}
+const ll maxn=500005;
+ll sum[maxn];
+ll ans[maxn];
 int main()
 {
-    std::ios::sync_with_stdio(false);
+    //std::ios::sync_with_stdio(false);
     #ifdef test
     freopen("F:\\Desktop\\question\\in.txt","r",stdin);
     #endif
@@ -25,104 +35,38 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n,m;
-    cin>>n>>m;
-    int i,j;
-    string s;
-    wfor(i,1,n+1)
+    ll t;
+    //cin>>t;
+    read(t);
+    while(t--)
     {
-        cin>>s;
-        wfor(j,1,m+1)
+        ll n,m;
+        //cin>>n>>m;
+        read(n);
+        read(m);
+        ll i;
+        sum[1]=0;
+        ans[1]=0;
+        wfor(i,0,n)
         {
-            ma[i][j]=s[j-1]-'0';
-            if(ma[i][j]==1)
-                sum[i][j]=sum[i][j-1]+1;
+            ll s,r,x,a;
+            read(r);
+            read(s);
+            read(x);
+            read(a);
+            //cin>>r>>s>>x>>a;
+            ll temp=s*ksm(r,mod-2);
+            sum[i+2]=((temp-1)*((ans[i+1]-ans[x]+mod)%mod)%mod+temp*a%mod)%mod;
+            ans[i+2]=sum[i+2]+ans[i+1];
+        }
+        wfor(i,0,m)
+        {
+            ll l,r;
+            //cin>>l>>r;
+            read(l);read(r);
+            printf("%lld\n",ans[r]-ans[l]);
+            //cout<<(ans[r]-ans[l]+mod)%mod<<endl;
         }
     }
-    wfor(j,1,m+1)
-    {
-        wfor(i,1,n+1)
-        {
-            if(ma[i][j]==1)
-                h[i][j]=h[i-1][j]+1;
-        }
-    }
-    int ans=0;
-    wfor(i,1,n+1)
-    {
-        int L[maxn]={0};
-        int R[maxn]={0};
-        stack<pair<int,int>>st;
-        wfor(j,1,m+1)
-        {
-            if(ma[i][j]!=1)
-            {
-                L[j]=-1;
-                R[j]=-1;
-                while(!st.empty())
-                {
-                    int pos=st.top().first;
-                    R[pos]=j;
-                    st.pop();
-                }
-            }else
-            {
-                if(!st.empty())
-                {
-                    if(h[i][j]>=st.top().second)
-                    {
-                        if(h[i][j]==st.top().second)
-                            L[j]=-1;
-                        else
-                        {
-                            L[j]=st.top().first;
-                        }
-                        st.push(make_pair(j,h[i][j]));
-                    }else
-                    {
-                        int temp=0;
-                        while(!st.empty()&&h[i][j]<st.top().second)
-                        {
-                            int pos=st.top().first;
-                            R[pos]=j;
-                            temp=L[pos];
-                            st.pop();
-                        }
-                        if(!st.empty()&&h[i][j]!=st.top().second)
-                        {
-                            L[j]=st.top().first;
-                        }
-                        else if(!st.empty())
-                        {
-                            L[j]=-1;
-                        }else
-                        {
-                            L[j]=temp;
-                        }
-                        st.push(make_pair(j,h[i][j]));
-                    }
-                }else
-                {
-                    L[j]=j-1;
-                    st.push(make_pair(j,h[i][j]));
-                }
-            }
-        }
-        while(!st.empty())
-        {
-            int pos=st.top().first;
-            R[pos]=j;
-            st.pop();
-        }
-        wfor(j,1,m+1)
-        {
-            if(L[j]!=-1)
-            {
-                if(sum[i][R[j]-1]-sum[i][L[j]]!=sum[i+1][R[j]-1]-sum[i+1][L[j]])
-                    ans++;
-            }
-        }
-    }
-    cout<<ans<<endl;
     return 0;
 }
