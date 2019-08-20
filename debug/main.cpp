9c4fc23599eac0f0a6eec95e54f52548c4c09956
wy-time <1142942494@qq.com>
2019-08-20 19:53:43 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -9,6 +10,26 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+int sum[2][4][2][120];
+void slove()
+{
+    memset(sum,0,sizeof(sum));
+    int i,j,k;
+    wfor(i,0,2)
+    {
+        wfor(j,0,4)
+        {
+            wfor(k,0,2)
+            {
+                int a,b,c;
+                wfor(a,0,i+1)
+                    wfor(b,0,j+1)
+                        wfor(c,0,k+1)
+                            sum[i][j][k][a*10+b*20+c*50]=1;
+            }
+        }
+    }
+}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -23,9 +44,57 @@ int main()
     cin>>t;
     while(t--)
     {
-        int n,m,k;
-        cin>>n>>m>>k;
-        cout<<m+1+(k-1)*(m/(n-k+1)+1)<<endl;
+        slove();
+        int num[105];
+        int n;
+        cin>>n;
+        int i,j,k;
+        int no=0;
+        wfor(i,0,n)
+        {
+            cin>>num[i];
+            if(num[i]%10!=0)
+                no=1;
+        }
+        if(no==1)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        ll ans=1e9;
+        wfor(i,0,2)
+        {
+            wfor(j,0,4)
+            {
+                wfor(k,0,2)
+                {
+                    int p;
+                    int cnt=0;
+                    int flag=1;
+                    wfor(p,0,n)
+                    {
+                        if(sum[i][j][k][num[p]%100]!=1&&(num[p]<=110&&sum[i][j][k][num[p]]!=1))
+                        {
+                            flag=0;
+                            break;
+                        }else
+                        {
+                            int fix=0;
+                            if(num[p]%100==0&&sum[i][j][k][100]==1)
+                                fix++;
+                            if(num[p]%110==0&&sum[i][j][k][110]==1)
+                                fix++;
+                            cnt=max(cnt,num[p]/100-fix);
+                        }
+                    }
+                    if(flag)
+                    {
+                        ans=min(ans,(ll)i+j+k+cnt);
+                    }
+                }
+            }
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
