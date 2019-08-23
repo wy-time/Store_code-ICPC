91a142fa83a58a2562f1070b61bf8be5f5be6cd2
wy-time <1142942494@qq.com>
2019-08-23 21:55:18 +0800

    hdu

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
-#include <string> 
+#include <algorithm> 
+#include <queue> 
+#include <vector> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-// void read(ll &x) {
+// void read(int &x) {
 // 	char ch = getchar(); x = 0;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
+const int maxn=5e4+5;
+vector<pair<ll,int>>G[maxn];
+struct st
+{
+    int u;
+    ll w;
+    int id;
+    bool operator <(const st&other)const
+    {
+        return w>other.w;
+    }
+};
+ll ans[maxn];
+int question[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -20,52 +36,57 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    ll t;
+    int t;
     cin>>t;
     while(t--)
     {
-        ll n;
-        cin>>n;
-        ll a,b;
-        cin>>a>>b;
-        string s;
-        cin>>s;
-        ll i;
-        ll ans=n*a+2*b;
-        char last='0';
-        wfor(i,1,n)
+        int n,m,q;
+        cin>>n>>m>>q;
+        int i;
+        wfor(i,0,n+2)
         {
-            if(s[i]!=last)
-            {
-                ans+=2*b+a;
-            }else
-                ans+=(last-'0'+1)*b;
-            last=s[i];
+            G[i].clear();
         }
-        last='0';
-        ll sub=0;
-        ll add=0;
-        ll flag=0;
-        wfor(i,1,n)
+        priority_queue<st>qu;
+        wfor(i,0,m)
         {
-            if(flag&&s[i]==last)
-                add+=b;
-            if(s[i]!=last&&last=='1')
+            int u,v;
+            ll w;
+            cin>>u>>v>>w;
+            G[u].push_back(make_pair(w,v));
+        }
+        wfor(i,1,n+1)
+        {
+            sort(G[i].begin(),G[i].end());
+            if(G[i].size()>0)
+                qu.push({i,G[i][0].first,0});
+        }
+        int maxnum=0;
+        wfor(i,0,q)
+        {
+            cin>>question[i];
+            maxnum=max(maxnum,question[i]);
+        }
+        int cnt=0;
+        while(!qu.empty())
+        {
+            st temp=qu.top();
+            qu.pop();
+            ans[++cnt]=temp.w;
+            if(cnt>maxnum)
+                break;
+            if(temp.id+1<G[temp.u].size())
             {
-                sub+=a;
-                flag=1;
-            }else if(s[i]!=last&&flag)
-            {
-                flag=0;
-                sub+=a;
-                if(sub>add)
-                    ans-=sub-add;
-                sub=0;
-                add=0;
+                qu.push({temp.u,temp.w-G[temp.u][temp.id].first+G[temp.u][temp.id+1].first,temp.id+1});
             }
-            last=s[i];
+            int v=G[temp.u][temp.id].second;
+            if(G[v].size()>0)
+                qu.push({v,temp.w+G[v][0].first,0});
         }
-        cout<<ans<<endl;
+        wfor(i,0,q)
+        {
+            cout<<ans[question[i]]<<endl;
+        }
     }
     return 0;
 }
