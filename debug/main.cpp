d5a7c6bee21ca41763adc86d5fbddb0ff2291867
wy-time <1142942494@qq.com>
2019-08-24 20:01:34 +0800

    cf

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <vector> 
-#include <algorithm> 
-#include <map> 
+#include <bitset> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -12,28 +10,6 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=3e5+5;
-pair<int,int>num[maxn];
-map<pair<int,int>,int >ma;
-vector<pair<int,int>>v;
-const ll mod=998244353;
-ll fac[maxn];
-struct rule
-{
-    bool operator ()(const pair<int,int> &a,const pair<int,int>&b)
-    {
-        return a.second<b.second;
-    }
-};
-void init()
-{
-    ll i;
-    fac[0]=1;
-    wfor(i,1,maxn)
-    {
-        fac[i]=fac[i-1]*i%mod;
-    }
-}
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -44,66 +20,28 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    int n;
-    cin>>n;
     int i;
-    init();
-    wfor(i,0,n)
+    cout<<"? ";
+    wfor(i,0,99)
     {
-        cin>>num[i].first>>num[i].second;
-        ma[num[i]]++;
-        v.push_back(num[i]);
+        cout<<(i<<7)<<" ";
     }
-    sort(v.begin(),v.end());
-    v.erase(unique(v.begin(),v.end()),v.end());
-    sort(num,num+n);
-    int cnt=1;
-    ll ans1=1;
-    ll ans2=1;
-    int flag=0;
-    wfor(i,1,n)
+    cout<<(i<<7)<<endl;
+    int fir;
+    cin>>fir;
+    cout<<"? ";
+    wfor(i,0,99)
+        cout<<i<<" ";
+    cout<<i<<endl;
+    int sec;
+    cin>>sec;
+    bitset<14>firnum(fir);
+    bitset<14>senum(sec);
+    mfor(i,13,7)
     {
-        if(num[i].second<num[i-1].second)
-        {
-            flag=1;
-            break;
-        }
+        firnum[i]=senum[i];
     }
-    wfor(i,1,n)
-    {
-        if(num[i].first==num[i-1].first)
-            cnt++;
-        else
-        {
-            ans1=ans1*fac[cnt]%mod;
-            cnt=1;
-        }
-    }
-    ans1=ans1*fac[cnt]%mod;
-    sort(num,num+n,rule());
-    cnt=1;
-    wfor(i,1,n)
-    {
-        if(num[i].second==num[i-1].second)
-            cnt++;
-        else
-        {
-            ans2=ans2*fac[cnt]%mod;
-            cnt=1;
-        }
-    }
-    ans2=ans2*fac[cnt]%mod;
-    if(flag==0)
-    {
-        ll temp=1;
-        for(auto k:v)
-            temp=temp*fac[ma[k]]%mod;
-        ll ans=(ans1+ans2-temp+mod)%mod;
-        cout<<(fac[n]-ans+mod)%mod<<endl;;
-    }else
-    {
-        ll ans=(ans1+ans2)%mod;
-        cout<<(fac[n]-ans+mod)%mod<<endl;
-    }
+    ll ans=firnum.to_ullong();
+    cout<<"! "<<ans<<endl;
     return 0;
 }
