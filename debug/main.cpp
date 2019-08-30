341b1698f18f6b222cc8b35551b6c4d3e808ea1c
wy-time <1142942494@qq.com>
2019-08-30 20:56:24 +0800

    vj

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
-#include <string> 
-#include <cstring> 
+#include <map> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
-void read(int &x) {
-char ch = getchar(); x = 0;
-for (; ch < '0' || ch > '9'; ch = getchar());
-for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
-}
-const int maxn=250005;
+// void read(int &x) {
+// 	char ch = getchar(); x = 0;
+// 	for (; ch < '0' || ch > '9'; ch = getchar());
+// 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
+// }
+const int maxn=90005;
+int tot,last;
 struct st
 {
     int len;
     int link;
-    int _next[26];
-    int cnt;
+    int num;
+    map<char,int>_next;
+    void init()
+    {
+        len=0;
+        link=-1;
+        _next.clear();
+        tot=1;
+        last=0;
+        num=0;
+    }
 };
-st sam[maxn*2];
-int tot,last;
-void init()
-{
-    sam[tot].len=0;
-    sam[tot].link=-1;
-    memset(sam[tot]._next,0,sizeof(sam[tot]._next));
-    sam[tot].cnt=1;
-    tot=1;
-    last=0;
-};
+st sam[maxn];
 void add(char c)
 {
     int cur=tot++;
     sam[cur].len=sam[last].len+1;
-    sam[cur].cnt=1;
-    int p;
-    for(p=last;p!=-1&&!sam[p]._next[c-'a'];p=sam[p].link)
-        sam[p]._next[c-'a']=cur;
+    sam[cur].num=0;
+    int p=last;
+    for(;p!=-1&&!sam[p]._next.count(c);p=sam[p].link)
+        sam[p]._next[c]=cur;
     if(p==-1)
         sam[cur].link=0;
     else
     {
-        int q=sam[p]._next[c-'a'];
+        int q=sam[p]._next[c];
         if(sam[p].len+1==sam[q].len)
             sam[cur].link=q;
         else
@@ -50,21 +49,38 @@ void add(char c)
             int clone=tot++;
             sam[clone]=sam[q];
             sam[clone].len=sam[p].len+1;
-            sam[clone].cnt=0;
-            for(;p!=-1&&sam[p]._next[c-'a']==q;p=sam[p].link)
-                sam[p]._next[c-'a']=clone;
-            sam[q].link=clone;
-            sam[cur].link=clone;
+            for(;p!=-1&&sam[p]._next[c]==q;p=sam[p].link)
+                sam[p]._next[c]=cur;
+            sam[q].link=sam[cur].link=clone;
         }
     }
     last=cur;
 }
-int toop[2*maxn];
 int sum[maxn];
-int ans[maxn];
+int toop[maxn*2];
+string ans="";
+void slove(int now,int aim)
+{
+    if(aim<=0)
+        return;
+    for(auto k:sam[now]._next)
+    {
+        if(sam[k.second].num<aim)
+        {
+            aim-=sam[k.second].num;
+            continue;
+        }else
+        {
+            ans+=k.first;
+            aim--;
+            slove(k.second,aim);
+            break;
+        }
+    }
+}
 int main()
 {
-    //std::ios::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
     #ifdef test
     freopen("F:\\Desktop\\question\\in.txt","r",stdin);
     #endif
@@ -72,39 +88,32 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    init();
-    char s[maxn];
-    scanf("%s",s);
-    int len=strlen(s);
+    string s;
+    cin>>s;
+    sam[0].init();
     int i;
-    wfor(i,0,len)
+    wfor(i,0,s.length())
         add(s[i]);
     wfor(i,0,tot)sum[sam[i].len]++;
-    wfor(i,1,len+1)sum[i]+=sum[i-1];
+    wfor(i,1,s.length()+1)sum[i]+=sum[i-1];
     wfor(i,0,tot)toop[--sum[sam[i].len]]=i;
     mfor(i,tot-1,0)
     {
         int v=toop[i];
-        int fa=sam[v].link;
-        if(fa!=-1)
-            sam[fa].cnt+=sam[v].cnt;
+        for(auto k:sam[v]._next)
+            sam[v].num+=sam[k.second].num;
+        sam[v].num++;
     }
-    wfor(i,0,tot)
+    sam[0].num--;
+    int q;
+    cin>>q;
+    while(q--)
     {
-        if(sam[i].link!=-1)
-        {
-            int j;
-            wfor(j,sam[sam[i].link].len+1,sam[i].len+1)
-                ans[j]=max(ans[j],sam[i].cnt);
-        }else
-        {
-            int j;
-            wfor(j,1,sam[i].len+1)
-                ans[j]=max(ans[j],sam[i].cnt);
-        }
+        int num;
+        cin>>num;
+        ans="";
+        slove(0,num);
+        cout<<ans<<endl;
     }
-    wfor(i,0,len)
-        printf("%d\n",ans[i+1]);
-        //cout<<ans[i+1]<<endl;
     return 0;
 }
