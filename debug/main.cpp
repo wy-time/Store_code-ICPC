bf7c3668ac8a4d037eafbf5a32e8d492bd5f93d5
wy-time <1142942494@qq.com>
2019-08-30 16:05:50 +0800

    vj

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstring> 
 #include <string> 
+#include <cstring> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -11,14 +11,13 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=100005;
+const int maxn=250005;
 struct st
 {
     int len;
     int link;
     int _next[26];
-    int mx;
-    int ans;
+    int cnt;
 };
 st sam[maxn*2];
 int tot,last;
@@ -26,18 +25,16 @@ void init()
 {
     sam[tot].len=0;
     sam[tot].link=-1;
-    sam[tot].mx=0;
-    sam[tot].ans=-1;
     memset(sam[tot]._next,0,sizeof(sam[tot]._next));
-    tot++;
+    sam[tot].cnt=1;
+    tot=1;
     last=0;
-}
+};
 void add(char c)
 {
     int cur=tot++;
     sam[cur].len=sam[last].len+1;
-    sam[cur].ans=sam[cur].len;
-    sam[cur].mx=0;
+    sam[cur].cnt=1;
     int p;
     for(p=last;p!=-1&&!sam[p]._next[c-'a'];p=sam[p].link)
         sam[p]._next[c-'a']=cur;
@@ -53,6 +50,7 @@ void add(char c)
             int clone=tot++;
             sam[clone]=sam[q];
             sam[clone].len=sam[p].len+1;
+            sam[clone].cnt=0;
             for(;p!=-1&&sam[p]._next[c-'a']==q;p=sam[p].link)
                 sam[p]._next[c-'a']=clone;
             sam[q].link=clone;
@@ -63,6 +61,7 @@ void add(char c)
 }
 int toop[2*maxn];
 int sum[maxn];
+int ans[maxn];
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -79,40 +78,31 @@ int main()
     int i;
     wfor(i,0,s.length())
         add(s[i]);
-    wfor(i,0,tot) sum[sam[i].len]++;
-    wfor(i,1,s.length()+1) sum[i]+=sum[i-1];
-    wfor(i,0,tot) toop[--sum[sam[i].len]]=i;
-    while(cin>>s)
+    wfor(i,0,tot)sum[sam[i].len]++;
+    wfor(i,1,s.length()+1)sum[i]+=sum[i-1];
+    wfor(i,0,tot)toop[--sum[sam[i].len]]=i;
+    mfor(i,tot-1,0)
     {
-        int p=0,l=0;
-        wfor(i,0,s.length())
-        {
-            while(p&&!sam[p]._next[s[i]-'a'])
-            {
-                p=sam[p].link;
-                l=sam[p].len;
-            }
-            if(sam[p]._next[s[i]-'a']!=0)
-            {
-                p=sam[p]._next[s[i]-'a'];
-                l++;
-                sam[p].mx=max(sam[p].mx,l);
-            }
-        }
-        mfor(i,tot-1,0)
-        {
-            int v=toop[i];
-            if(sam[v].link!=-1)
-                sam[sam[v].link].mx=max(sam[sam[v].link].mx,min(sam[v].mx,sam[sam[v].link].len));
-            sam[v].ans=min(sam[v].ans,sam[v].mx);
-            sam[v].mx=0;
-        }
+        int v=toop[i];
+        int fa=sam[v].link;
+        if(fa!=-1)
+            sam[fa].cnt+=sam[v].cnt;
     }
-    int res=0;
     wfor(i,0,tot)
     {
-        res=max(res,sam[i].ans);
+        if(sam[i].link!=-1)
+        {
+            int j;
+            wfor(j,sam[sam[i].link].len+1,sam[i].len+1)
+                ans[j]=max(ans[j],sam[i].cnt);
+        }else
+        {
+            int j;
+            wfor(j,1,sam[i].len+1)
+                ans[j]=max(ans[j],sam[i].cnt);
+        }
     }
-    cout<<res<<endl;
+    wfor(i,0,s.length())
+        cout<<ans[i+1]<<endl;
     return 0;
 }
