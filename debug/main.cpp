9da0c0e06c693515bba81655a530f2809b5e689f
wy-time <1142942494@qq.com>
2019-08-31 18:35:26 +0800

    vj

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <map> 
+#include <cstring> 
+#include <vector> 
 #include <cstdio>
 using namespace std;
 typedef long long ll;
@@ -10,75 +11,89 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=90005;
-int tot,last;
+vector<string>s;
+vector<string>t;
+const int maxn=100005;
 struct st
 {
     int len;
     int link;
-    int num;
-    map<char,int>_next;
+    int _next[26];
+    int cnt;
 };
+int tot,last;
 st sam[2*maxn];
-void init()
+void init(int id)
 {
-    sam[tot].len=0;
-    sam[tot].link=-1;
-    sam[tot]._next.clear();
-    tot=1;
-    last=0;
-    sam[tot].num=0;
+    sam[id].len=sam[id].cnt=0;
+    sam[id].link=-1;
+    memset(sam[id]._next,0,sizeof(sam[id]._next));
 }
 void add(char c)
 {
     int cur=tot++;
+    init(cur);
     sam[cur].len=sam[last].len+1;
-    sam[cur].num=0;
+    sam[cur].cnt=1;
     int p=last;
-    for(;p!=-1&&!sam[p]._next.count(c);p=sam[p].link)
-        sam[p]._next[c]=cur;
+    last=cur;
+    for(;p!=-1&&!sam[p]._next[c-'a'];p=sam[p].link)
+        sam[p]._next[c-'a']=cur;
     if(p==-1)
         sam[cur].link=0;
     else
     {
-        int q=sam[p]._next[c];
+        int q=sam[p]._next[c-'a'];
         if(sam[p].len+1==sam[q].len)
             sam[cur].link=q;
         else
         {
             int clone=tot++;
+            init(clone);
             sam[clone]=sam[q];
             sam[clone].len=sam[p].len+1;
-            for(;p!=-1&&sam[p]._next[c]==q;p=sam[p].link)
-                sam[p]._next[c]=cur;
-            sam[q].link=sam[cur].link=clone;
+            for(;p!=-1&&sam[p]._next[c-'a']==q;p=sam[p].link)
+                sam[p]._next[c-'a']=clone;
+            sam[cur].link=sam[q].link=clone;
         }
     }
-    last=cur;
 }
-int sum[maxn];
 int toop[maxn*2];
-string ans="";
-void slove(int now,int aim)
+void slove(int id,int ti)
 {
-    if(aim<=0)
-        return;
-    for(auto k:sam[now]._next)
+    int sum[maxn]={0};
+    init(0);
+    int i;
+    wfor(i,0,s[id].length())
+        add(s[id][i]);
+    wfor(i,0,tot)sum[sam[i].len]++;
+    wfor(i,1,s[id].length()+1)sum[i]+=sum[i-1];
+    wfor(i,0,tot)toop[--sum[sam[i].len]]=i;
+    mfor(i,tot-1,0)
     {
-        if(aim<=0)
-            return ;
-        if(sam[k.second].num<aim)
+        int v=toop[i];
+        if(sam[v].link!=-1)
+            sam[sam[v].link].cnt+=sam[v].cnt;
+    }
+    int now=0;
+    wfor(i,0,t[ti].length())
+    {
+        if(now==-1)
+            break;
+        if(sam[now]._next[t[ti][i]-'a']!=0)
         {
-            aim-=sam[k.second].num;
+            now=sam[now]._next[t[ti][i]-'a'];
             continue;
         }else
         {
-            ans+=k.first;
-            aim--;
-            slove(k.second,aim);
-            return ;
+            now=sam[now].link;
+            i--;
         }
     }
+    if(now==-1||t[ti].length()==0)
+        cout<<0<<endl;
+    else
+        cout<<sam[now].cnt<<endl;
 }
 int main()
 {
@@ -90,31 +105,49 @@ int main()
     freopen("/home/time/debug/debug/in","r",stdin);
     freopen("/home/time/debug/debug/out","w",stdout);
     #endif
-    string s;
-    cin>>s;
-    init();
-    int i;
-    wfor(i,0,s.length())
-        add(s[i]);
-    wfor(i,0,tot)sum[sam[i].len]++;
-    wfor(i,1,s.length()+1)sum[i]+=sum[i-1];
-    wfor(i,0,tot)toop[--sum[sam[i].len]]=i;
-    mfor(i,tot-1,0)
-    {
-        int v=toop[i];
-        for(auto k:sam[v]._next)
-            sam[v].num+=sam[k.second].num;
-        sam[v].num++;
-    }
     int q;
     cin>>q;
+    s.clear();
+    t.clear();
+    s.push_back("");
+    t.push_back("");
     while(q--)
     {
-        int num;
-        cin>>num;
-        ans="";
-        slove(0,num);
-        cout<<ans<<endl;
+        int op;
+        cin>>op;
+        char c;
+        int si,ti,op2;
+        string temp;
+        switch(op)
+        {
+            case 1:
+                cin>>si>>c;
+                si--;
+                temp=s[si]+c;
+                s.push_back(temp);
+                break;
+            case 2:
+                cin>>op2>>ti>>c;
+                ti--;
+                if(op2==0)
+                    temp=c+t[ti];
+                else
+                    temp=t[ti]+c;
+                t.push_back(temp);
+                break;
+            case 3:
+                int ti2;
+                cin>>ti>>ti2;
+                ti2--;ti--;
+                t.push_back(t[ti]+t[ti2]);
+                break;
+            case 4:
+                cin>>ti>>si;
+                ti--,si--;
+                tot=1;last=0;
+                slove(si,ti);
+                break;
+        }
     }
     return 0;
 }
