2befbbb8a79337931ad6365208c4bcca1e953882
wy-time <1142942494@qq.com>
2019-10-13 19:24:13 +0800

    cf

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-#include <bitset> 
-#include <cstdio>
+#include <bits/stdc++.h> 
 using namespace std;
-typedef long long ll;
+typedef __int128 ll;
 #define wfor(i,j,k) for(i=j;i<k;++i)
 #define mfor(i,j,k) for(i=j;i>=k;--i)
 // void read(int &x) {
@@ -10,108 +8,135 @@ typedef long long ll;
 // 	for (; ch < '0' || ch > '9'; ch = getchar());
 // 	for (; ch >= '0' && ch <= '9'; ch = getchar()) x = x * 10 + ch - '0';
 // }
-const int maxn=1e5+5;
-struct EDGE
+ll exgcd(ll a, ll b, ll &x, ll &y)
 {
-    int w;
-    int end;
-    int _next;
-};
-struct node
-{
-    int _next[2];
-    void init()
-    {
-        _next[0]=_next[1]=0;
-    }
-};
-node tree[55];
-EDGE edge[maxn];
-int head[maxn];
-int cnt;
-void add(int beg,int end,int w)
-{
-    edge[++cnt]._next=head[beg];
-    edge[cnt].end=end;
-    edge[cnt].w=w;
-    head[beg]=cnt;
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	ll g = exgcd(b, a % b, x, y);
+	ll t = x;
+	x = y;
+	y = t - a / b * y;
+	return g;
 }
-int num[maxn];
-void dfs(int now,int res)
-{
-    int i;
-    for(i=head[now];i;i=edge[i]._next)
-    {
-        int v=edge[i].end;
-        res^=edge[i].w;
-        num[v]=res;
-        dfs(v,res);
-        res^=edge[i].w;
+inline __int128 read(){
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9'){
+        if(ch == '-')
+            f = -1;
+        ch = getchar();
     }
+    while(ch >= '0' && ch <= '9'){
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
 }
-int tot=0;
-void char_insert(string c)
-{
-    int i;
-    wfor(i,0,c.length())
-    {
-        int now=0;
-        if(tree[now]._next[c[i]-'0']==0)
-        {
-            tree[++tot].init();
-            tree[now]._next[c[i]-'0']=tot;
-        }
-        now=tot;
+inline void print(__int128 x){
+    if(x < 0){
+        putchar('-');
+        x = -x;
     }
+    if(x > 9)
+        print(x / 10);
+    putchar(x % 10 + '0');
 }
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    #ifdef test
-    freopen("F:\\Desktop\\question\\in.txt","r",stdin);
-    #endif
-    #ifdef ubuntu
-    freopen("/home/time/debug/debug/in","r",stdin);
-    freopen("/home/time/debug/debug/out","w",stdout);
-    #endif
-    int n;
-    cin>>n;
-    int i;
-    wfor(i,0,n)
-    {
-        int u,v,w;
-        cin>>u>>v>>w;
-        add(u,v,w);
-    }
-    num[1]=0;
-    dfs(1,0);
-    wfor(i,1,n+1)
-    {
-        bitset<32>bit (num[i]);
-        string s=bit.to_string();
-        char_insert(s);
-    }
-    unsigned long long ans=0;
-    wfor(i,1,n+1)
-    {
-        bitset<32>bit (num[i]);
-        string s=bit.to_string();   
-        int j;
-        wfor(j,31,0)
-        {
-            int now=0;
-            if(tree[now]._next[(s[j]-'0')^1]!=0)
-            {
-                bit[j]=1;
-                now=tree[now]._next[(s[j]-'0')^1];
-            }else
-            {
-                now=tree[now]._next[(s[j]-'0')];
-                bit[j]=0;
-            }
-        }
-        ans=max(ans,bit.to_ullong());
-    }
-    cout<<ans<<endl;
-    return 0;
+	//std::ios::sync_with_stdio(false);
+#ifdef test
+	freopen("F:\\Desktop\\question\\in.txt", "r", stdin);
+#endif
+#ifdef ubuntu
+	freopen("/home/time/debug/debug/in", "r", stdin);
+	freopen("/home/time/debug/debug/out", "w", stdout);
+#endif
+	ll n, p, w, d;
+    n=read(),p=read(),w=read(),d=read();
+	ll x, y;
+	ll g = exgcd(w, d, x, y);
+	if (p % g != 0)
+        print(-1);
+		//cout << -1 << endl;
+	else
+	{
+		x = x * p / g;
+		y = y * p / g;
+		ll kx = d / g;
+		ll ky = w / g;
+		int flag = 1;
+		if (x < 0)
+		{
+			ll temp = -x;
+			temp = temp / kx + (temp % kx != 0);
+			x += temp * kx;
+			y -= temp * ky;
+			if (y < 0)
+				flag = 0;
+		}
+		if (flag && y < 0)
+		{
+			ll temp = -y;
+			temp = temp / ky + (temp % ky != 0);
+			y += temp * ky;
+			x -= temp * kx;
+			if (x < 0)
+				flag = 0;
+		}
+		if (flag)
+		{
+			if (x + y <= n)
+                print(x),printf(" "),print(y),printf(" "),print(n-x-y),printf("\n");
+			else
+			{
+				if (kx > ky)
+				{
+					ll cnt = x / kx;
+					x -= cnt * kx;
+					y += cnt * ky;
+				} else
+				{
+					ll cnt = y / ky;
+					y -= cnt * ky;
+					x += cnt * kx;
+				}
+				if (x < 0)
+				{
+					ll temp = -x;
+					temp = temp / kx + (temp % kx != 0);
+					x += temp * kx;
+					y -= temp * ky;
+					if (y < 0)
+						flag = 0;
+				}
+				if (flag && y < 0)
+				{
+					ll temp = -y;
+					temp = temp / ky + (temp % ky != 0);
+					y += temp * ky;
+					x -= temp * kx;
+					if (x < 0)
+						flag = 0;
+				}
+				if (flag)
+				{
+					if (x + y <= n)
+                        print(x),printf(" "),print(y),printf(" "),print(n-x-y),printf("\n");
+					else
+                        print(-1);
+						//cout << -1 << endl;
+				} else
+                    print(-1);
+					//cout << -1 << endl;
+			}
+		} else
+            print(-1);
+			//cout << -1 << endl;
+	}
+	return 0;
 }
+
